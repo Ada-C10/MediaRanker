ebc96b8b5fdea6032a1143716e7190e49c1bd580
Daniela Di Iorio <danielad@gmail.com>
2018-10-08 16:37:57 -0700

    validates title is present and unique within category

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,7 +1,6 @@
 require "test_helper"
 require 'pry'
 describe Work do
-  let(:work) { Work.new }
   let(:secret) {works(:secret)}
   let(:remember) {works(:remember)}
   let(:billy) {works(:billy)}
@@ -10,59 +9,90 @@ describe Work do
     expect(remember.valid?).must_equal true
   end
 
-  it "only allows valid categories" do
-    expect(secret.valid?).must_equal true  #book
-    expect(remember.valid?).must_equal true  #album
-    expect(billy.valid?).must_equal true  #movie
+  describe "category validation" do
+    it "only allows valid categories" do
+      expect(secret.valid?).must_equal true  #book
+      expect(remember.valid?).must_equal true  #album
+      expect(billy.valid?).must_equal true  #movie
 
-    secret.category = nil
-    expect(secret.valid?).must_equal false
+      secret.category = nil
+      expect(secret.valid?).must_equal false
 
-    secret.category = 1
-    expect(secret.valid?).must_equal false
+      secret.category = 1
+      expect(secret.valid?).must_equal false
 
-    secret.category = 'marshmallow'
-    expect(secret.valid?).must_equal false
+      secret.category = 'marshmallow'
+      expect(secret.valid?).must_equal false
+    end
   end
 
-  it "allows publication_year to be blank" do
-    expect(billy.valid?).must_equal true
+  describe "publication_year validation" do
+    it "allows publication_year to be blank" do
+      expect(billy.valid?).must_equal true
 
-    billy.publication_year = nil
-    expect(billy.valid?).must_equal true
+      billy.publication_year = nil
+      expect(billy.valid?).must_equal true
 
-    billy.publication_year = ""
-    expect(billy.valid?).must_equal true
+      billy.publication_year = ""
+      expect(billy.valid?).must_equal true
 
-    billy.publication_year = " "
-    expect(billy.valid?).must_equal true
+      billy.publication_year = " "
+      expect(billy.valid?).must_equal true
 
-    billy.publication_year = "        "
-    expect(billy.valid?).must_equal true
+      billy.publication_year = "        "
+      expect(billy.valid?).must_equal true
+    end
+
+    it "only allows publication_year between 1000 and current year" do
+      expect(billy.valid?).must_equal true
+
+      billy.publication_year = 'a word'
+      expect(billy.valid?).must_equal false
+
+      billy.publication_year = 0
+      expect(billy.valid?).must_equal false
+
+      billy.publication_year = 3035
+      expect(billy.valid?).must_equal false
+
+      billy.publication_year = 999
+      expect(billy.valid?).must_equal false
+
+      billy.publication_year = 1000
+      expect(billy.valid?).must_equal true
+
+      billy.publication_year = 2018
+      expect(billy.valid?).must_equal true
+
+      billy.publication_year = Date.today.year
+      expect(billy.valid?).must_equal true
+    end
   end
 
-  it "only allows publication_year between 1000 and current year" do
-    expect(billy.valid?).must_equal true
+  describe "title validation" do
+    it "checks for unique titles within categories" do
+      #copy secret's title and category
+      secret_copy = Work.new( title: secret.title,
+                              category: secret.category,
+                              creator: 'someone else',
+                              publication_year: 1111,
+                              description: 'a fake book')
+      expect(secret_copy.valid?).must_equal false
+      expect(secret_copy.errors.messages).must_include :title
 
-    billy.publication_year = 'a word'
-    expect(billy.valid?).must_equal false
+      #copy secret's title, but use a different category
+      billy.title = secret.title
+      expect(billy.valid?).must_equal true
+    end
 
-    billy.publication_year = 0
-    expect(billy.valid?).must_equal false
+    it "must have a title" do
+      remember.title = nil
+      expect(remember.valid?).must_equal false
+      expect(remember.errors.messages).must_include :title
 
-    billy.publication_year = 3035
-    expect(billy.valid?).must_equal false
-
-    billy.publication_year = 999
-    expect(billy.valid?).must_equal false
-
-    billy.publication_year = 1000
-    expect(billy.valid?).must_equal true
-
-    billy.publication_year = 2018
-    expect(billy.valid?).must_equal true
-
-    billy.publication_year = Date.today.year
-    expect(billy.valid?).must_equal true
+      remember.title = " "
+      expect(remember.valid?).must_equal false
+      expect(remember.errors.messages).must_include :title
+    end
   end
 end
