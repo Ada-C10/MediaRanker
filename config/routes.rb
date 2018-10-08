eac97c68466ceeed7d5bb9b6598bf193ac9ed9bb
Hannah <hannahvanseeters@gmail.com>
2018-10-08 14:12:21 -0700

    generated controller for users, votes, and works

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'votes/index'
+  get 'works/index'
+  get 'works/show'
+  get 'users/index'
+  get 'users/show'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
