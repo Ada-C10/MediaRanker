151aa2e23b48b4235af64d818a4cbf90cc276eb6
Melissa O'Hearn <melissa.ohearn@gmail.com>
2018-10-08 16:37:25 -0700

    add tables to migration

--- db/migrate/20181008233029_create_works.rb
+++ db/migrate/20181008233029_create_works.rb
@@ -1,6 +1,11 @@
 class CreateWorks < ActiveRecord::Migration[5.2]
   def change
     create_table :works do |t|
+      t.string :title
+      t.string :category
+      t.string :creator
+      t.date :pub_date
+      t.string :description
 
       t.timestamps
     end
