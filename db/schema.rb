7c4b91af57257097613b13653b6a431686adabc2
Aurea <fu.sakura@gmail.com>
2018-10-08 16:29:28 -0700

    add seeding

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_08_220612) do
+ActiveRecord::Schema.define(version: 2018_10_08_232107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,11 +36,11 @@ ActiveRecord::Schema.define(version: 2018_10_08_220612) do
     t.string "title"
     t.string "category"
     t.string "creator"
-    t.date "pub_year"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "vote_id"
+    t.integer "pub_year"
     t.index ["vote_id"], name: "index_works_on_vote_id"
   end
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,11 +6,8 @@ describe User do
 
     it 'invalid without a name' do
       user = User.first
-
       user.name = nil
-
       expect( user.valid? ).must_equal false
-
     end
 
     it 'is valid when name is present' do
@@ -19,16 +16,14 @@ describe User do
   end
 
   describe 'Relations' do
+
     it 'can add a vote using method votes' do
       user = User.first
       work = Work.first
       vote = Vote.new(work: work)
 
-
       user.votes << vote
       expect( user.votes.first ).must_equal vote
     end
   end
-
-
 end
