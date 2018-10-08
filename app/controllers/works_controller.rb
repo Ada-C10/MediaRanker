685655c209ee5dc9b77c7ab6e3bfc3d50bfbc6c9
Katricia Smith <smithkatricia@gmail.com>
2018-10-08 16:24:51 -0700

    Add Works controller create and read actions

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,32 @@
 class WorksController < ApplicationController
+
+  def index
+    @works = Work.all
+  end
+
+
+  def new
+    @work = Work.new
+  end
+
+
+  def create
+    @work = Work.new(work_params)
+
+    if @work.save
+      redirect_to works_path
+    else
+      render :new
+    end
+  end
+
+
+  def show
+    @work = Work.find_by(id: params[:id])
+
+    if @work.nil?
+      head :not_found
+    end 
+  end
+
 end
