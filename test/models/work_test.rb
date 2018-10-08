95493fbd449e4857cfca71bb67b14a8e7d318531
Laura <larmartinez2@gmail.com>
2018-10-08 16:19:42 -0700

    create test for validating title:

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -8,10 +8,55 @@ describe Work do
   end
 
   it 'has required fields' do
-  fields = [:title, :category, :creator, :publication_year, :description]
+    fields = [:title, :category, :creator, :publication_year, :description]
 
-  fields.each do |field|
-    expect(work).must_respond_to field
+    fields.each do |field|
+      expect(work).must_respond_to field
+    end
   end
+
+  describe 'validations' do
+    it 'must have a title' do
+      # Arrange
+      work = works(:book)
+      work.title = nil
+
+      # Act
+      #valid = book.valid?
+      valid = work.save
+
+      # Assert
+      expect(valid).must_equal false
+      expect(work.errors.messages).must_include :title
+    end
+
+    it 'requires a unique title' do
+      other_work = works(:album)
+      #change the title to the work title we set above (work points to book in the test data)
+      other_work.title = work.title
+
+      valid = other_work.valid?
+
+      expect(valid).must_equal false
+      expect(other_work.errors.messages).must_include :title
+    end
   end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
