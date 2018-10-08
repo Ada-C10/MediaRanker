8d7a36d4990fcab13014630c467a220bd314ee34
Leanne Rivera <rivera.leanne+ada@gmail.com>
2018-10-08 16:19:08 -0700

    validations added to Work Model

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,8 @@
 class Work < ApplicationRecord
+  validates :title, presence: true, uniqueness: true
+  validates :creator, presence: true
+  validates :year, presence: true
+  validates :description, presence: true
+  validates :category, presence: true
+
 end
