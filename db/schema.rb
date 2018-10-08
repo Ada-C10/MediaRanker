4dba26f70d1451703be51af00b10ca69c08b2667
Lindsay Terchin <lterchin@gmail.com>
2018-10-08 16:11:34 -0700

    created model for votes w/ foreign key for user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_08_223546) do
+ActiveRecord::Schema.define(version: 2018_10_08_230953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,4 +21,12 @@ ActiveRecord::Schema.define(version: 2018_10_08_223546) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "votes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
+  end
+
+  add_foreign_key "votes", "users"
 end
