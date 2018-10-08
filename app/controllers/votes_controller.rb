d2e0d891ec6324656b16c471d09092a474d17212
Carly Reiter <yepodacarly@gmail.com>
2018-10-08 16:15:54 -0700

    added initial controller methods for Votes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,29 @@
+require 'date'
+
 class VotesController < ApplicationController
+
+  def index
+    @votes = Vote.all
+  end
+
+  def show
+    @vote = Trip.find_by(id: params[:id])
+    if @vote.nil?
+      head :not_found
+    end
+  end
+
+  def new
+    @vote = Vote.new
+  end
+
+  def create
+    @vote = Vote.new
+    if @vote.save
+      redirect_to votes_path
+    else
+      render :new
+    end
+  end
+
 end
