c0d218b9edcbcca537f44a7274965ead18240961
Lindsay Terchin <lterchin@gmail.com>
2018-10-08 16:19:14 -0700

    created model for work with corresponding foreign keys

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_08_230953) do
+ActiveRecord::Schema.define(version: 2018_10_08_231548) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,8 +25,20 @@ ActiveRecord::Schema.define(version: 2018_10_08_230953) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.bigint "work_id"
     t.index ["user_id"], name: "index_votes_on_user_id"
+    t.index ["work_id"], name: "index_votes_on_work_id"
+  end
+
+  create_table "works", force: :cascade do |t|
+    t.string "category"
+    t.string "title"
+    t.date "publication_year"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "votes", "users"
+  add_foreign_key "votes", "works"
 end
