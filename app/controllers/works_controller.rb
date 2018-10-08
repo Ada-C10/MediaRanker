8e21f356e30836e142cd91c20c8e08e9766ffeb7
xLJohnsonx <lizjohnsonemail@gmail.com>
2018-10-08 14:53:48 -0700

    create Works contoller

---
+++ app/controllers/works_controller.rb
@@ -0,0 +1,19 @@
+class WorksController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'works/index'
+  get 'works/show'
+  get 'works/update'
+  get 'works/create'
+  get 'works/new'
+  get 'works/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
