6fe3a75fb16c6d22ff44a0a7d8e3984095aaae80
Carly Reiter <yepodacarly@gmail.com>
2018-10-08 15:59:30 -0700

    Added initial routing to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :votes
+
+
+  resources :works do
+    resources :votes, only: [:index, :create, :update]
+  end
+
+
+  resources :users do
+    resources :votes, only: [:index, :create]
+  end
+
+
 end
