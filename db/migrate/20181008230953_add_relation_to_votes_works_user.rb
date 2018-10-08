cc0762ed657585ccd39dfbd292118ca2d4399bc1
Semret Nicodimos <snicodimos@gmail.com>
2018-10-08 16:35:50 -0700

    created relations with tables and ran migrations

--- db/migrate/20181008230953_add_relation_to_votes_works_user.rb
+++ db/migrate/20181008230953_add_relation_to_votes_works_user.rb
@@ -1,4 +1,6 @@
 class AddRelationToVotesWorksUser < ActiveRecord::Migration[5.2]
   def change
+    add_reference :votes, :users, foreign_key: true
+    add_reference :votes, :works, foreign_key: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_08_230331) do
+ActiveRecord::Schema.define(version: 2018_10_08_230953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,10 @@ ActiveRecord::Schema.define(version: 2018_10_08_230331) do
   create_table "votes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "users_id"
+    t.bigint "works_id"
+    t.index ["users_id"], name: "index_votes_on_users_id"
+    t.index ["works_id"], name: "index_votes_on_works_id"
   end
 
   create_table "works", force: :cascade do |t|
@@ -36,4 +40,6 @@ ActiveRecord::Schema.define(version: 2018_10_08_230331) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "votes", "users", column: "users_id"
+  add_foreign_key "votes", "works", column: "works_id"
 end
