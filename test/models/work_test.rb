63cbefe89c701ed6b328534c664a80826ab5dc12
Laura <larmartinez2@gmail.com>
2018-10-08 15:59:35 -0700

    create test data and validation test

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,9 +1,17 @@
 require "test_helper"
 
 describe Work do
-  let(:work) { Work.new }
+  let(:work) { works(:book) }
 
   it "must be valid" do
     value(work).must_be :valid?
+  end
+
+  it 'has required fields' do
+  fields = [:title, :category, :creator, :publication_year, :description]
+
+  fields.each do |field|
+    expect(work).must_respond_to field
+  end
   end
 end
