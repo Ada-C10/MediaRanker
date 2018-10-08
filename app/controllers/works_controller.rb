e7639320543a274c60f102b0754ce3d1545b1eff
Danielle Metzner <danimetzner@gmail.com>
2018-10-08 15:21:14 -0700

    Added action methods to the controller

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,7 +1,46 @@
 class WorksController < ApplicationController
   def index
+    @works = Work.all.order(:title)
   end
 
   def show
+    id = params[:id]
+    @work = Work.find_by(id: id)
+
+    if @work.nill?
+      render :notfound, status: :not_found
+    end
+  end
+
+  def edit
+    @work = Work.find_by(id: params[:id])
+  end
+
+  def create
+    @work = Work.new(work_params)
+    if @work.save
+      redirect_to work_path(@work.id)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @work = Work.find_by(id: params[:id])
+    if @work.update(work_params)
+      redirect_to work_path(@work.id)
+    end
+  end
+
+  def destroy
+    work = Work.find_by(id: params[:id])
+    work.destroy
+    redirect_to works_path
+  end
+
+  private
+
+  def work_params
+    return params.require(:work).permit(:title)
   end
 end
