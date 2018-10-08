4a0a6f688da155f7df1165aa0628d4ebe008750a
Carly Reiter <yepodacarly@gmail.com>
2018-10-08 16:23:51 -0700

    Added basic controller setup action for User.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,36 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+    if @user.nil?
+      head :not_found
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to users_path
+    else
+      render :new, status: :bad_request
+    end
+  end
+
+  private
+
+  def user_params
+    return params.require(:user).permit(
+      :username,
+    )
+  end
+
 end
