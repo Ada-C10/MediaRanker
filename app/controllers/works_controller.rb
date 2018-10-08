e7bbfe2b02ad8240f5fc7cf23509f258fd9b5890
Chantelle Belic <chantelle.belic@gmail.com>
2018-10-08 16:33:25 -0700

    added work #index#new#show#create logic, need to add views, else logic

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,3 +1,37 @@
 class WorksController < ApplicationController
-  
+
+  def index
+    @works = Work.all
+  end
+
+  def show
+    work_id = params[:id]
+    @work = Work.find_by(id: work_id)
+    #if nil render not found, else show page
+  end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    filtered_params = work_params()
+    work = Work.new(filtered_params)
+    #if nil re-render form, else  show all
+  end
+
+  private
+
+  #strong params
+  def work_params
+    return params.require(:work).permit(
+      :category,
+      :title,
+      :creator,
+      :publication_year,
+      :description
+    )
+  end
+
+
 end
