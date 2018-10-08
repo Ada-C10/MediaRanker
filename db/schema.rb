0233467edf271ef3a08a9734a46ae8df2afa207c
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-10-08 16:00:59 -0700

    Added Work has_many :users through :votes association

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_08_220823) do
+ActiveRecord::Schema.define(version: 2018_10_08_225613) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "string"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -25,6 +24,10 @@ ActiveRecord::Schema.define(version: 2018_10_08_220823) do
   create_table "votes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "work_id"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
+    t.index ["work_id"], name: "index_votes_on_work_id"
   end
 
   create_table "works", force: :cascade do |t|
@@ -37,4 +40,6 @@ ActiveRecord::Schema.define(version: 2018_10_08_220823) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "votes", "users"
+  add_foreign_key "votes", "works"
 end
