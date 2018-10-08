fe9ee47a658e93d9749905317a8e9355117355d2
Valerie Gidzinski <valgidzi@gmail.com>
2018-10-08 15:49:04 -0700

    Add BooksController#index action and view template

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,35 @@
 class WorksController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def work_params
+    return params.require(:work).permit(
+      :category,
+      :title,
+      :creator,
+      :pub_year,
+      :description
+    )
+  end
 end
