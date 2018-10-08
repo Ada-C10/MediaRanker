a110c0ea116d54218449b0c52eee22b82366ef47
Katie Jahanmir <katietj@gmail.com>
2018-10-08 16:27:50 -0700

    completed database seeding

--- config/routes.rb
+++ config/routes.rb
@@ -3,3 +3,13 @@ Rails.application.routes.draw do
 resources :works
 
 end
+
+
+#works GET    /works(.:format)                                                                         works#index
+                #           POST   /works(.:format)                                                                         works#create
+                #  new_work GET    /works/new(.:format)                                                                     works#new
+                # edit_work GET    /works/:id/edit(.:format)                                                                works#edit
+                #      work GET    /works/:id(.:format)                                                                     works#show
+                #           PATCH  /works/:id(.:format)                                                                     works#update
+                #           PUT    /works/:id(.:format)                                                                     works#update
+                #           DELETE /works/:id(.:format)                                                                     works#destroy
