d7ba553a81941f280fa80dee3e846dbf1ca4a1e3
Aurea <fu.sakura@gmail.com>
2018-10-08 16:39:40 -0700

    implement basic work tests

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,4 @@
 class Work < ApplicationRecord
+  validates :title, presence: true, uniqueness: true
+  has_many :votes
 end
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,9 +1,34 @@
 require "test_helper"
 
 describe Work do
-  let(:work) { Work.new }
+  describe 'Validation' do
 
-  it "must be valid" do
-    value(work).must_be :valid?
+    it 'invalid without a title' do
+      work = Work.first
+      work.title = nil
+      expect( work.valid? ).must_equal false
+    end
+
+    it 'invalid with a nonunique title' do
+      work = Work.new(title: Work.first.title)
+      expect( work.valid? ).must_equal false
+    end
+
+    it 'is valid when title is present and unique' do
+      work = Work.new(title: 'test title')
+      expect( work.valid? ).must_equal true
+    end
+  end
+
+  describe 'Relations' do
+
+    it 'can add a vote using method votes' do
+      work = Work.first
+      user = User.first
+      vote = Vote.new(user: user)
+
+      work.votes << vote
+      expect( work.votes.first ).must_equal vote
+    end
   end
 end
