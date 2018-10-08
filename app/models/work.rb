00bfff0e4e9d6d3eaac1d0563c5d5839b893064d
Daniela Di Iorio <danielad@gmail.com>
2018-10-08 15:32:30 -0700

    work validates 4-digit year

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,5 +1,5 @@
 class Work < ApplicationRecord
 
   validates :title, presence: true, uniqueness: { scope: [:category] }
-
+  validates :publication_year, inclusion: { in: 1000..Date.today.year, message: "should be a four-digit year"}
 end
