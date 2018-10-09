866ad36f2fbffe3c262dba2d67443c5a6aa9c393
Pushpa Agr <pushpaagr108@gmail.com>
2018-10-08 21:46:42 -0700

    added brief data to users.yml and works.yml

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,6 +1,14 @@
 class WorksController < ApplicationController
 
   def index
+    @works = Work.all
+  end
+
+  def show
+    @work = Work.find_by( id: params[:id])
+    if @work.nil?
+      head :not_found
+    end
   end
 
   
---
+++ db/migrate/20181009005009_change_date_from_string_to_date.rb
@@ -0,0 +1,6 @@
+class ChangeDateFromStringToDate < ActiveRecord::Migration[5.2]
+  def change
+    change_column :works, :publication_year, :integer, using: 'publication_year::integer'
+
+  end
+end
