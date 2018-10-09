8f0f93515218ae25b71c0c7cbd482da462926211
Sigrid Benezra <sdbenezra@gmail.com>
2018-10-09 12:35:46 -0700

    Correct home route in routes file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get get '/home', to: 'home#index'
+  get '/home', to: 'home#index'
   root to: 'home#index'
 
   resources :works
