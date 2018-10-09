db07505aab1674db663d565700a813e030635826
Jazz <jazziesf21@yahoo.com>
2018-10-09 10:52:25 -0700

    checkout views branch

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   resources :votes
 
   resources :users
-
+  post 'user/login' to: 'user#login', as: 'login'
+  
   resources :works
 
+
+# get 'sessions/new to: session#new sessions
+# get sessions destroy to sessions destory as logout
 end
