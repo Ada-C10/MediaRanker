b24187e76ec5a6c744550c940e369e03d70d77c6
Semret Nicodimos <snicodimos@gmail.com>
2018-10-08 22:45:33 -0700

    added validation tests for work and user

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,15 +1,36 @@
 require "test_helper"
 
 describe Work do
+  # describe 'relations' do
+  #   it 'a vote can be set through the method work' do
+  #     vote = Vote.create!
+  #     work = Work.new()
+  #
+  #     work.vote = vote
+  #
+  #     expect(vote.work).must_equal work
+  #     epect(vote.work_id).must_equal work_id
+  #   end
+  # end
 
-  describe '' do
+  describe 'Validations' do
+    before do
+      @work = Work.first
+    end
     it 'is valid when title is present' do
-      work = Work.new(title: 'Some title')
 
-
-      is_valid = work.valid?
+      is_valid = @work.valid?
 
       expect(is_valid).must_equal true
+    end
+
+    it 'is invalid without a title' do
+      @work.title = nil
+
+      is_valid = @work.valid?
+
+      expect(is_valid).must_equal false
+      expect(@work.errors.messages).must_include :title
     end
   end
 end
