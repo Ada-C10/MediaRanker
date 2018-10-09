5aabfac86809acabe6600e523076e36bcf7ce20f
Maryam Shitu <maryamshitu@gmail.com>
2018-10-08 18:00:43 -0700

    Works controller with basic CRUD operation created, routes created, views not yet created

---
+++ app/controllers/works_controller.rb
@@ -0,0 +1,49 @@
+class WorksController < ApplicationController
+  def index
+    @works = Work # .order(:name)
+  end
+
+  def show
+    @work = Work.find(params[:id].to_i)
+  end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new(work_params)
+
+    if @work.save
+      # redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @work = Work.find(params[:id].to_i)
+  end
+
+  def update
+    @work = Work.find_by(id: params[:id].to_i)
+
+    if @work.update(work_params)
+      redirect_to work_path(@work.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    work = Work.find_by(id: params[:id].to_i)
+    @deleted_work = work.destroy
+  end
+
+  private
+
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+  end
+
+end
