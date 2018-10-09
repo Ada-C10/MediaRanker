35216344b75ae3982cd832dc3af181f8cf85dd11
Sigrid Benezra <sdbenezra@gmail.com>
2018-10-08 23:09:36 -0700

    Correct name of publication year column in works.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_08_231616) do
+ActiveRecord::Schema.define(version: 2018_10_09_060609) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2018_10_08_231616) do
     t.string "category"
     t.string "title"
     t.string "creator"
-    t.string "year_published"
+    t.string "publication_year"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
