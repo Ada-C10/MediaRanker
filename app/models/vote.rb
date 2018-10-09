521494d738515bd13816f0f3c809c0ec5ee213b3
Hannah <hannahvanseeters@gmail.com>
2018-10-09 14:48:45 -0700

    changed application header

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -4,3 +4,17 @@ class Vote < ApplicationRecord
 
   validates :quantity, numericality: { only_integer: true, greater_than: 0 }
 end
+
+#upvote
+##edge cases
+###one user trying to upvote twice
+# 1. test where the user already has a vote for that work
+# 2. test where the user does not have a vote for that work
+# (the model does not track who has not been logged in)
+
+#works sorted by vote
+# .order(votes)
+## edge cases
+### if there are no works in a category
+### if there's no votes in a category
+### tie between 2 pieces of work
