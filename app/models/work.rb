944c4b2c0b6f1d1708dfd02c1f61d87490a282a6
Trang <trang.frego@gmail.com>
2018-10-08 19:40:05 -0700

    create Vote model

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,14 @@
 class Work < ApplicationRecord
+  def self.movies_list
+    return Work.select { |work| work.category == 'movie' }
+  end
+
+  def self.books_list
+    return Work.select { |work| work.category == 'book' }
+  end
+
+  def self.albums_list
+    return Work.select { |work| work.category == 'album' }
+  end
+  
 end
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -6,4 +6,33 @@ describe Work do
   it "must be valid" do
     value(work).must_be :valid?
   end
+
+  describe 'validations' do
+    it 'must have a category' do
+    end
+
+    it 'must have a title' do
+    end
+
+    it 'must have a creator' do
+    end
+
+    it 'must have a publication year' do
+    end
+
+    it 'must have a description' do
+    end
+  end
+
+  describe 'relationships' do
+  end
+
+  describe 'movies_list' do
+  end
+
+  describe 'books_list' do
+  end
+
+  describe 'albums_list' do
+  end
 end
