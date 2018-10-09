732ab5a59baf846c2e8fbc11a7a534fe7c056a80
Lindsay Terchin <lterchin@gmail.com>
2018-10-09 16:46:40 -0700

    created show view and controller method for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
 
   def index
+    @users = User.all
   end
-  
+
+
+  def show
+    @user = User.find_by(id: params[:id])
+    if @user.nil?
+      head :not_found
+    end
+  end
+
 end
