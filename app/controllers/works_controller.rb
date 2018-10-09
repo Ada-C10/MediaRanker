c3e666db49a4ff0c0d25c694bacac01e597a40dd
Sigrid Benezra <sdbenezra@gmail.com>
2018-10-09 10:33:46 -0700

    Setup show page for work, create link from home index page to work show page.

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,4 +2,15 @@ class WorksController < ApplicationController
   def index
     @works = Work.all
   end
+
+  def show
+    @work = Work.find_by(id: params[:id])
+  end
+
+
+  private
+
+  def work_params
+    params.require(:work).permit(:categor, :title, :creator, :publication_year, :description)
+  end
 end
