fbe45d6d83870c31777f941313f29b3c428a329d
jackie <jacquelyn.cheng@me.com>
2018-10-09 16:25:36 -0700

    Added Works#edit Works#update and Works#destroy and setup respective views

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -19,19 +19,44 @@ class WorksController < ApplicationController
     @work = Work.new(work_params)
 
     if @work.save
+      flash[:success] = "Work added"
       redirect_to work_path(@work.id)
     else
+      flash.now[:error] = "Error: Work not added"
+      @work.errors.messages.each do |field, messages|
+        flash.now[field] = messages
+      end
+
       render :new
     end
   end
 
   def edit
+    @work = Work.find_by(id: params[:id].to_i)
   end
 
   def update
+    @work = Work.find_by(id: params[:id].to_i)
+    if @work.update(work_params)
+      flash[:success] = "Changes saved"
+      redirect_to work_path(@work.id)
+    else
+      flash.now[:error] = "Error: Changes not saved"
+      @work.errors.messages.each do |field, messages|
+        flash.now[field] = messages
+      end
+
+      render :edit
+    end
   end
 
   def destroy
+    work = Work.find_by(id: params[:id].to_i)
+    deleted_work = work.destroy
+
+    flash[:success] = "#{deleted_work.title} deleted"
+
+    redirect_to root_path
   end
 
   private
