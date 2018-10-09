7e60742ad90d708e59b9a03ce8cdaba2da54e26d
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-10-09 10:44:53 -0700

    Started moving things to best place in MVC

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -3,6 +3,16 @@ class WorksController < ApplicationController
   end
 
   def index
+    @section_headers = []
+    @work_titles = []
+# Find project constants in config/initializers/constants.rb
+    VALID_WORK_CATEGORIES.each do |cat|
+      @section_headers << "ALL" + "#{cat.upcase}" + "S"
+      Work.by_category(cat).each do |wrk|
+        @work_titles << wrk.title
+      end
+    end
+
   end
 
   def show
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,4 +2,6 @@ class Vote < ApplicationRecord
   belongs_to :work
   belongs_to :user
   # belongs_to triggers validation error by default
+
+  scope
 end
