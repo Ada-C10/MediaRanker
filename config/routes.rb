bb6c9047b690c333b3f15e027fa1a787bde274d1
Hannah <hannahvanseeters@gmail.com>
2018-10-09 14:55:37 -0700

    session controller created as well as routes for login, new, and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  post 'sessions/login', to: 'sessions#login', as: 'login'
+  get 'sessions/new', to: 'session#new', as: 'new_session'
+  get 'sessions/destroy', to: 'session#destroy', as: 'logout'
+  # need to update below to exclude routes we do not need
   resources :works
   resources :users
+  resources :votes
   root to: 'pages#home'
-  get 'votes/index'
-  get 'works/index'
-  get 'works/show'
-  get 'users/index'
-  get 'users/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
