d40027f651e25f9488696181a0a9ecfa10b4b03c
Kay <kayxn23@gmail.com>
2018-10-08 19:10:00 -0700

    created migration to add pubyear and category columns to works  and removed votes column from works

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_08_230039) do
+ActiveRecord::Schema.define(version: 2018_10_09_020033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "works", force: :cascade do |t|
-    t.integer "votes"
     t.string "title"
     t.string "creator"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
+    t.integer "publication_year"
   end
 
 end
