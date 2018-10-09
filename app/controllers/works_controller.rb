d97e7f9296f60b90dbd2acd074a4b4365de72425
Daniela Di Iorio <danielad@gmail.com>
2018-10-08 17:10:09 -0700

    works index shows separate tables per media type

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -3,6 +3,10 @@ class WorksController < ApplicationController
 
   def index
     @works = Work.all
+    @books = Work.select {|work| work.category = 'book'}
+    @albums = Work.select {|work| work.category = 'album'}
+    @movies = Work.select {|work| work.category = 'movie'}
+
   end
 
   def show
