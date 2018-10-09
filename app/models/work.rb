efe18746e3440138583b1f374b5c11c3f3180c6f
Hannah <hannahvanseeters@gmail.com>
2018-10-08 20:36:02 -0700

    tests for validation and relationships created for work

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,10 @@
 class Work < ApplicationRecord
+  has_many :votes
+
+  validates :title, uniqueness: true, presence: true
+  validates :creator, presence: true
+  validates :publication_year, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :description, presence: true
+  validates :category, presence: true
+
 end
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,9 +1,84 @@
 require "test_helper"
 
 describe Work do
-  let(:work) { Work.new }
+  let(:work) { works(:wheel_of_time) }
 
   it "must be valid" do
     value(work).must_be :valid?
+  end
+
+  describe 'Relationships' do
+    it 'has many votes' do
+      expect(work.votes).must_be_kind_of Integer
+      work.votes.must_equal votes(:wheel_of_time)
+    end
+  end
+
+  describe 'validations' do
+    it 'must have a title' do
+      work.title = nil
+
+      valid = work.valid?
+
+      expect(valid).must_equal false
+      expect(work.errors.messages).must_include :title
+    end
+
+    it 'must have a unique title' do
+      work2 = works(:harry_potter)
+      work2.title = work.title
+
+      valid = work2.valid?
+
+      expect(valid).must_equal false
+      expect(work2.errors.messages).must_include :title
+    end
+
+    it 'must have a creator' do
+      work.creator = nil
+
+      valid = work.valid?
+
+      expect(valid).must_equal false
+      expect(work.errors.messages).must_include :creator
+    end
+
+    it 'must have a year published' do
+      work.publication_year = nil
+
+      valid = work.valid?
+
+      expect(valid).must_equal false
+      expect(work.errors.messages).must_include :publication_year
+    end
+
+    it 'must have a numerical year published' do
+      work.publication_year = 'seaweed'
+
+      valid = work.valid?
+
+      expect(valid).must_equal false
+      expect(work.errors.messages).must_include :publication_year
+    end
+
+    it 'must have a category' do
+      #is this the correct test for a select drop down?
+      work.category = nil
+
+      valid = work.valid?
+
+      expect(valid).must_equal false
+      expect(work.errors.messages).must_include :category
+    end
+
+    it 'must have a description' do
+      work.description = nil
+
+      valid = work.valid?
+
+      expect(valid).must_equal false
+      expect(work.errors.messages).must_include :description
+    end
+
   end
 end
