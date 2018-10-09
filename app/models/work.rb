34f78d4bcc705d4372464901d71b09dfb04cf4bd
Chantelle Belic <chantelle.belic@gmail.com>
2018-10-09 12:28:12 -0700

    did validations for work category and title, but not uniqueness of title per category

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,4 @@
 class Work < ApplicationRecord
   validates :title, presence: true
-  validates_inclusion_of :category, :in => %w( movie book album )
+  validates_inclusion_of :category, :in => %w( movie book album MOVIE BOOK ALBUM )
 end
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,24 +1,71 @@
 require "test_helper"
 
+require 'pry'
+
 describe Work do
-  it 'must instantiate a valid book' do
-    work = Work.new(title: "a work", category: "book")
-    is_valid = work.valid?
-    expect(is_valid).must_equal true
+
+  before do
+    @work = Work.new(title: "a work", category: "book")
+  end
+
+  describe 'initialize ' do
+    it 'must instantiate a valid book' do
+      is_valid = @work.valid?
+      expect(is_valid).must_equal true
+    end
   end
 
   describe 'validations' do
-    it 'is valid when a title is present' do
+    it 'is valid when a title and a valid category of movie, book, or album is present' do
 
       work = Work.first
+      empty_hash = {}
       expect(work.valid?).must_equal true
+      expect(work.errors.messages).must_equal empty_hash
+    end
 
-    # messages = :book_example.errors.messages
-    # expect(messages).must_equal []
+    it 'is is invalid if category and title are nil' do
+
+      @work.title = nil
+      @work.category = nil
+
+      expect(@work.valid?).must_equal false
+      expect(@work.errors.messages).must_include :title, :category
     end
+
+    it 'is is invalid if category is not movie, book, album' do
+
+      @work.category = "film"
+      @work.title = "a work"
+      expect(@work.valid?).must_equal false
+      expect(@work.errors.messages).must_include :category
+      expect(@work.errors.messages).wont_include :title
+    end
+
+    it 'is is invalid without a category' do
+
+      @work.category = nil
+      expect(@work.valid?).must_equal false
+      expect(@work.errors.messages).must_include :category
+      expect(@work.errors.messages).wont_include :title
+    end
+
     it 'is is invalid without a title' do
+
+      @work.title = nil
+      @work.category = "book"
+      expect(@work.valid?).must_equal false
+
+      expect(@work.errors.messages).must_include :title
+      expect(@work.errors.messages).wont_include :category
     end
-    it 'is invalid if category is not book, movie, or album' do
+
+    it 'is invalid if work of same category and title is created' do
+      #TO DO: populate her
+    end
+
+    it 'is invalid if work of different category but same title is created' do
+      #TO DO: populate her
     end
   end
 end
