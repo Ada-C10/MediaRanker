233f9b2ea2d6ef225e8dcc0ceb12cc929bdd6513
Semret Nicodimos <snicodimos@gmail.com>
2018-10-08 18:19:33 -0700

    created seed.rb and seeded work/media data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+MEDIA_FILE = Rails.root.join('db', 'media_seeds.csv')
+puts "Loading raw trip data from #{MEDIA_FILE}"
+
+media_failures = []
+CSV.foreach(MEDIA_FILE, :headers => true) do |row|
+  media = Work.new
+  media.category = row['category']
+  media.title = row['title']
+  media.creator = row['creator']
+  media.publication_year = row['publication_year']
+  media.description = row['description']
+
+  successful = media.save
+  if !successful
+    media_failures << media
+    puts "Failed to save media: #{media.inspect}"
+  else
+    puts "Created media: #{media.inspect}"
+  end
+end
+
+puts "Added #{Work.count} media records"
+puts "#{media_failures.length} medium failed to save"
+puts "done"
