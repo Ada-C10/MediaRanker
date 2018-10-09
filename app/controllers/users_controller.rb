a17cd5885f340a268e9cd821b2fc118b097d3e4c
kangazoom <michellelcronin@gmail.com>
2018-10-08 19:29:31 -0700

    flesh out work+user controllers, add blank view files, and work model validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,74 @@
 class UsersController < ApplicationController
+
+    def index
+      # @user = User.all.order(:due_date)
+        @users = User.all
+    end
+
+    def show
+      if @user = User.find_by(id: params[:id].to_i)
+      else
+        return head :not_found
+      end
+    end
+
+    # def new
+    #   @user = User.new
+    # end
+    #
+    # def create
+    #   @user = User.new(user_params)
+    #
+    #   result = @user.save
+    #
+    #   if result
+    #     redirect_to users_path
+    #   else
+    #     render :new
+    #   end
+    # end
+    #
+    # def edit
+    #   @user = User.find_by(id: params[:id])
+    #   if !@user
+    #     return head :not_found
+    #   end
+    # end
+    #
+    # def update
+    #   @user = User.find(params[:id])
+    #
+    #   result = @user.update(user_params)
+    #
+    #   if result
+    #     redirect_to user_path(@user)
+    #   else
+    #     render :edit
+    #   end
+    # end
+    #
+    # def destroy
+    #
+    #   @user = User.find_by(id: params[:id])
+    #
+    #   if @user
+    #     @user.destroy
+    #     redirect_to users_path
+    #   else
+    #     return head :not_found
+    #   end
+    # end
+
+
+    private
+
+    def user_params
+      return params.require(:user).permit(
+        :username,
+      )
+    end
+  end
+
+  end
+
 end
--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,2 +1,74 @@
 class WorksController < ApplicationController
+
+  def index
+    # @work = Work.all.order(:due_date)
+      @works = Work.all.order(:category)
+  end
+
+  def show
+    if @work = Work.find_by(id: params[:id].to_i)
+    else
+      return head :not_found
+    end
+  end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new(work_params)
+
+    result = @work.save
+
+    if result
+      redirect_to works_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @work = Work.find_by(id: params[:id])
+    if !@work
+      return head :not_found
+    end
+  end
+
+  def update
+    @work = Work.find(params[:id])
+
+    result = @work.update(work_params)
+
+    if result
+      redirect_to work_path(@work)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+
+    @work = Work.find_by(id: params[:id])
+
+    if @work
+      @work.destroy
+      redirect_to works_path
+    else
+      return head :not_found
+    end
+  end
+
+
+  private
+
+  def work_params
+    return params.require(:work).permit(
+      :title,
+      :creator,
+      :publication_year,
+      :description,
+      :category
+    )
+  end
 end
