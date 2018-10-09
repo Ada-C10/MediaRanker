604aa24485c4db4edd132bb84dd6898f7c6d226d
Semret Nicodimos <snicodimos@gmail.com>
2018-10-08 23:41:14 -0700

    created user controller and some methods to go with it

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,39 @@
 class UsersController < ApplicationController
   def index
-  end 
+    @users = User.all.order(:username)
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+
+    if @user.nil?
+      head :not_found
+    end
+
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to users_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  
+  end
+
+  private
+  def user_params
+    return params.require(:user).permit(
+      :username
+    )
+  end
 end
