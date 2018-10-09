355662de99402ee641aa3bd9c68c4d351dbb2892
Semret Nicodimos <snicodimos@gmail.com>
2018-10-08 23:07:05 -0700

    works_control with the necessary methods

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,8 +1,61 @@
 class WorksController < ApplicationController
 
-def index
-end
+  def index
+    @works = Work.all.order(:title)
+  end
 
-def top
-end 
+  def show
+    @work = Work.find_by(id: params[:id])
+    if @work.nil?
+      head :not_found
+    end
+  end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+
+    @work = Work.new(work_params())
+
+    if @work.save
+      redirect_to work_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @work = Work.find_by(id: params[:id])
+  end
+
+  def update
+    work = Work.find_by(id: params[:id])
+
+    if work.update(work_params)
+      redirect_to work_path(work.id)
+    end
+  end
+
+  def destroy
+    @work = Work.find_by(id: params[:id])
+    @work.destroy
+
+    redirect_to works_path
+  end
+
+  def top
+  end
+
+  private
+  def work_params
+    return params.require(:work).permit(
+      :title,
+      :category,
+      :creator,
+      :publication_year,
+      :description
+    )
+  end
 end
