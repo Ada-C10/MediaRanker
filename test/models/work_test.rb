fa5ee638f4c2911b9f8b62383d619d9646f0ae53
Dionisia Montanez <dmontanez15@gmail.com>
2018-10-08 20:14:06 -0700

    title, presence, uniqueness validations to models/work.rb, fixed works.yml so tests did not have errors from migration, added tests to test/models/work_test.rb with 3 passing tests.

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,9 +1,30 @@
 require "test_helper"
 
-describe Work do
-  let(:work) { Work.new }
+# describe Work do
+#   let(:work) { Work.new }
+#
+#   it "must be valid" do
+#     value(work).must_be :valid?
+#   end
 
-  it "must be valid" do
-    value(work).must_be :valid?
+describe Work do
+  describe 'validations' do
+    before do
+      @work = Work.new(title: 'test work', publication_year: 2018, description: 'some description', creator: 'some creator', category: 'movie')
+    end
+
+    it 'is valid when all fields are present' do
+      valid_work = @work.valid?
+
+      expect(valid_work).must_equal true
+    end
+
+    it 'is invalid without a title' do
+      @work.title = nil
+      result = @work.valid?
+      expect(result).must_equal false
+      expect(@work.errors.messages).must_include :title
+    end
+
   end
 end
