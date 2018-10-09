c469ec880940d933ebc976da9075b0c327bb7059
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-10-08 19:27:17 -0700

    Created votes controller. Added nested routes for user and work to have access to associated votes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
-  get 'works/new'
-  get 'works/create'
-  get 'works/update'
-  get 'works/edit'
-  get 'works/show'
-  get 'works/destroy'
-  get 'works/index'
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'votes/index'
+  resources :work do
+    resources :votes, only:[:index]
+  end
+
+  resources :user do
+    resources :votes, only:[:index]
+  end
+
+  # Unsure if I need a vote controller/vote routes as it's linked to the work/user views
 end
