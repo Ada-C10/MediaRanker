fbe17627d920e4e1c00028dcba9181954ebc92cc
Katrina Aganon <katrina.aganon@gmail.com>
2018-10-09 15:57:16 -0700

    add test for unique title

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -3,13 +3,35 @@ require "test_helper"
 describe Work do
   describe 'validations' do
     it 'has a title' do
-      work = Work.new(title: nil, category: 'album', creator: 'test_creator', publication_year: 1989, description: 'this is the test description')
+      work = works(:beatles)
+
+      work.title = nil
 
       is_valid = work.valid?
 
       expect( is_valid ).must_equal false
     end
 
-  
+    it 'has a unique title' do
+      work = works(:potter)
+
+      new_work = Work.new(category: 'book', title: work.title, creator: 'Jon Snow', publication_year: 1990, description: 'Creating a new description here')
+
+      is_valid = new_work.valid?
+
+      expect( is_valid ).must_equal false
+
+    end
+
+    it 'is valid when all fields are present' do
+      work = works(:beatles)
+
+      is_valid = work.valid?
+
+      expect( is_valid ).must_equal true
+
+    end
+
+
   end
 end
