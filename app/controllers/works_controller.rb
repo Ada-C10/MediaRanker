a6ff52dcd5658630ba174c9d9a4b8a96df5ed660
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-10-09 01:58:05 -0700

    Basic form for new work set up

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -13,5 +13,22 @@ class WorksController < ApplicationController
   end
 
   def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new(work_params)
+    if @work.save
+      redirect_to work_path(@work.id)
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def work_params
+    return params.require(:work).permit(:title, :creator) #TODO: update permit
   end
 end
