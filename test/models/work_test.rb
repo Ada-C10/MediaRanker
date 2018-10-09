6b779f4287464b91da9264233c389084686f9283
Semret Nicodimos <snicodimos@gmail.com>
2018-10-08 22:22:26 -0700

    minitest wasn't working and had to reinstall everything and edit test_helper.rb, wrote one test for model for work

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,9 +1,15 @@
 require "test_helper"
 
 describe Work do
-  let(:work) { Work.new }
 
-  it "must be valid" do
-    value(work).must_be :valid?
+  describe '' do
+    it 'is valid when title is present' do
+      work = Work.new(title: 'Some title')
+
+
+      is_valid = work.valid?
+
+      expect(is_valid).must_equal true
+    end
   end
 end
