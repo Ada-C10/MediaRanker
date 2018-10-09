202d704725afd50fc938841768ce4d26b563ca58
Pushpa Agr <pushpaagr108@gmail.com>
2018-10-08 22:18:48 -0700

    added validations for user model and work model

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,10 @@
 class Work < ApplicationRecord
   has_many :votes
 
+  validates :title, presence: true, uniqueness: true
+  validates :category, presence: true
+  vaildates :creator, presence: true
+  validates :publication_year, presence: true
+  validates :description, presence: true
+  
 end
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,9 +1,33 @@
 require "test_helper"
 
-describe Work do
-  let(:work) { Work.new }
 
-  it "must be valid" do
-    value(work).must_be :valid?
+# create_table "works", force: :cascade do |t|
+#   t.string "title"
+#   t.string "category"
+#   t.string "creator"
+#   t.integer "publication_year"
+#   t.string "description"
+#   t.datetime "created_at", null: false
+#   t.datetime "updated_at", null: false
+# end
+
+
+describe Work do
+
+  describe 'relations' do
+    work = Work.create!(title: 'album')
+
+    # let(:work) { Work.new }
+    #
+    # it "must be valid" do
+    #   value(work).must_be :valid?
+    # end
+
+    it "must be valid when all fields are present" do
+      value(work).must_be :valid?
+    end
+
   end
+
+
 end
