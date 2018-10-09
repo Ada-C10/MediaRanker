6d46f9cb6be9b94efe88b5ceec1f1ce861e48358
Anibel America <ameriabel@gmail.com>
2018-10-09 16:30:26 -0700

    adds validation for work title

--- app/models/work.rb
+++ app/models/work.rb
@@ -2,13 +2,26 @@ class Work < ApplicationRecord
   has_many :votes
 
   # validations goes here: user and work combo is unique...
+  validates :title, presence: true, uniqueness: {scope: :creator, :message => "and creator combination already exists"}
 
+  CATEGORIES = %w[album book movie]
 
 
   def self.top_ten(category)
     return Work.where(category: category)
   end
 
+  def self.get_categories
+    return CATEGORIES
+  end
+
+
+  # Category (select)
+  # Title
+  # Creator
+  # Publication year
+  # Description (field)
+  # Submit "Create Work"
 
 
 
