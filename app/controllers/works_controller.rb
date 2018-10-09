422a2c038d675aeb904a39d8585e3891cbee0e90
Jazz <jazziesf21@yahoo.com>
2018-10-09 10:35:14 -0700

    add links to main page

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -41,9 +41,14 @@ class WorksController < ApplicationController
 
   def create
     @work = Work.new(work_params)
-    if @work.save # save returns true if the database insert succeeds
+    if @work.save
+      flash[:success] = "New work created"
       redirect_to work_path(@work.id) # go to the index so we can see the book in the list
     else # save failed :(
+      flash.now[:error] = 'Work not created'
+      @work.errors.messages.each do |field, messages|
+        flash.now[:field] = messages
+      end
       render :new # show the new book form view again
     end
   end
@@ -52,6 +57,7 @@ class WorksController < ApplicationController
     id = params[:id]
     @work = Work.find_by(id: id)
     if @work.destroy
+      flash[:success] = "#{work.title} deleted"
       redirect_to root_path
     end
   end
