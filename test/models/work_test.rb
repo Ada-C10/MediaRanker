b2d0be72986a2cb2bdaf024f4630c1099ffbf2cd
Sigrid Benezra <sdbenezra@gmail.com>
2018-10-09 16:32:51 -0700

    Add tests for work model validations.

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,9 +1,22 @@
 require "test_helper"
 
 describe Work do
-  let(:work) { Work.new }
+  let(:work) { Work.new(title: 'test') }
 
   it "must be valid" do
     value(work).must_be :valid?
+  end
+
+  describe 'validations' do
+    before do
+      @work = Work.new(
+        title: 'test title')
+    end
+
+    it 'is valid when title is present and unique' do
+
+      is_valid = @work.valid?
+      expect( is_valid ).must_equal true
+    end
   end
 end
