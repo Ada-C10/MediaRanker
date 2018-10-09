e762b7cbf1942e7456af55a2117e7e42af7e798f
Katricia Smith <smithkatricia@gmail.com>
2018-10-09 15:03:18 -0700

    Refactor routes to one line of resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :works
-  resources :users
-  resources :votes
+  resources :works, :users, :votes
 end
