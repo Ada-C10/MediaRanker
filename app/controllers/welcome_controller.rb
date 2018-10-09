112bcdb37aa7f9600a567f80c772a7bf298670e8
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-10-09 12:17:48 -0700

    Continued working on welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,15 @@
 class WelcomeController < ApplicationController
   def index
-    @works = Works.all
+    @works = Work.all
+    # SELECT all works for EACH category
+    # SHOW work title, publisher, votes
+    @movies = @works.select { |work| work.category == "movie" }
+    # @albums = @works.select { |work| work.category == "album" }
+    # @books = @works.select { |work| work.category == "book" }
+
+
     # Does not know what works are :(
     # Do I need to figure out the seed issue/seupt the controllers the rest of the way?
-    # Does it need a relationship to works? 
+    # Does it need a relationship to works?
   end
 end
