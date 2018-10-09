8041c667bfa96c720fd9f34a6dad674133f8d4ad
BarbaraWidjono <barbarawidjono@gmail.com>
2018-10-09 16:25:06 -0700

    Added validation tests for Works model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homepage#index'
-  resources :work, :user
+  resources :works, :users
 end
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,9 +1,29 @@
-require "test_helper"
+require 'test_helper'
+
 
 describe Work do
-  let(:work) { Work.new }
+  describe 'validations' do
 
-  it "must be valid" do
-    value(work).must_be :valid?
+    before do
+      @work = Work.new(title: 'casper babypants', category: 'album')
+    end
+
+    it 'is valid when title and category are present' do
+      is_valid = @work.valid?
+      expect(is_valid).must_equal true
+    end
+
+    it 'is not valid without a title' do
+      @work.title = nil
+      is_valid = @work.valid?
+      expect(is_valid).must_equal false
+    end
+
+    it 'is not valid without a category' do
+      @work.category = nil
+      is_valid = @work.valid?
+      expect(is_valid).must_equal false
+    end
+
   end
 end
