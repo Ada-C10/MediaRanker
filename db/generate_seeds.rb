ce320b966d4e54b50954f64781306a2d6d74c57d
Addison Cole <addison.cole85@gmail.com>
2018-10-09 16:32:36 -0700

    changed seed data

--- db/generate_seeds.rb
+++ db/generate_seeds.rb
@@ -13,13 +13,27 @@ require 'csv'
 CSV.open('db/media_seeds.csv', "w", :write_headers=> true,
   :headers => ["category", "title", "creator", "publication_year", "description"]) do |csv|
 
-  25.times do
-    category = %w(album book).sample
-    title = Faker::Coffee.blend_name
-    creator = Faker::Name.name
-    publication_year = rand(Date.today.year-100..Date.today.year)
-    description = Faker::Lorem.sentence
+    75.times do
+      category = %w(album book movie).sample
+      if category == "book"
+        title = Faker::HitchhikersGuideToTheGalaxy.starship
+        creator = Faker::HitchhikersGuideToTheGalaxy.character
+        publication_year = rand(Date.today.year-100..Date.today.year)
+        description = Faker::HitchhikersGuideToTheGalaxy.quote
+      end
+      if category == "album"
+        title = Faker::Music.album
+        creator = Faker::Music.band
+        publication_year = rand(Date.today.year-100..Date.today.year)
+        description = (Faker::Music.genre + " " + Faker::Music.instrument)
+      end
+      if category == "movie"
+        title = Faker::StarWars.call_squadron + " " + Faker::StarWars.specie + " " +  Faker::StarWars.vehicle
+        creator = Faker::StarWars.character
+        publication_year = rand(Date.today.year-100..Date.today.year)
+        description = Faker::Movie.quote
+      end
 
-    csv << [category, title, creator, publication_year, description]
+      csv << [category, title, creator, publication_year, description]
+    end
   end
-end
