e4493845101130aabe7cf4eea5ddfc541044d385
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-10-08 23:52:30 -0700

    Updated valid publication year for Vote model test

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -4,11 +4,11 @@ describe Vote do
   describe 'Vote validations - presence' do
      before do
        # Arrange
-       @user = User.create(name: 'test name')
-       @work = Work.create(title: 'test book', category: :album,
-                        creator: 'test creator', publication: 1234,
+       user = User.create(name: 'test name')
+       work = Work.create(title: 'test book', category: :album,
+                        creator: 'test creator', publication: 2018,
                         description: 'test description')
-       @vote = Vote.create(user_id: @user.id, work_id: @work.id)
+       @vote = Vote.create(user_id: user.id, work_id: work.id)
      end
 
      it 'is valid when all fields are present' do
