8793b5104c5a293b7d99476f1626a0ee131d4d05
BarbaraWidjono <barbarawidjono@gmail.com>
2018-10-08 20:42:33 -0700

    Edit form_with config

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,19 +14,3 @@ WORK.each do |row|
     work.save
     puts "#{work.title}"
 end
-
-# puts WORK
-
-# CSV.foreach(WORK_FILE, :headers => true) do |row|
-#   work = Work.new
-#   work.category = row['category']
-#   work.title = row['title']
-#   work.creator = row['creator']
-#   work.publication_year = row['publication_year']
-#   work.description = row['description']
-#
-# end
-
-# ActiveRecord::Base.connection.tables.each do |t|
-#   ActiveRecord::Base.connection.reset_pk_sequence!(t)
-# end
