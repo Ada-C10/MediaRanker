525af9d74c0bd6a4bd673df545269b4191c88e69
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-10-08 18:47:54 -0700

    Added relations to models and user/work id to votes model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  # Multiple votes can be associated with a single user
+  has_many :votes
+  # Can do user.votes for an array of all votes associated with the user
+  # Can push votes via user.votes << vote_object
+  # user.votes.where(conditions) to get votes for the user with the condition
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,2 +1,7 @@
 class Vote < ApplicationRecord
+  # A vote belongs to a single user, a single work
+  belongs_to :user
+  # Gives the option to do vote.user to get the user associated with the vote
+  belongs_to :work
+  # Gives the option to do vote.work to get the work associated with the vote 
 end
