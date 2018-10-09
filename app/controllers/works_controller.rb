f7ab33db7405bff763d67e8373cfd0a34ba54e45
Dionisia Montanez <dmontanez15@gmail.com>
2018-10-08 20:36:41 -0700

    created index method in controller and updated the views in the index.html.erb

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,4 +1,21 @@
 class WorksController < ApplicationController
   def index
+    if params[:title]
+      work = Work.find_by(id: params[:title])
+      @works = work.titles
+    else
+      @works = Work.all
+    end
+  end
+
+  private
+  def work_params
+    return params.require(:work).permit(
+      :title,
+      :publication_year,
+      :description,
+      :creator,
+      :category
+    )
   end
 end
