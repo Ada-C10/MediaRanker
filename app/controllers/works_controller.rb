10557854b0ee66e9768bdc4e7d2dd738a7b969a4
Katrina Aganon <katrina.aganon@gmail.com>
2018-10-08 22:56:47 -0700

    add user and votes controller and models

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -5,7 +5,26 @@ class WorksController < ApplicationController
 
   def show
     @work = Work.find_by(id: params[:id])
-    
   end
 
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new(work_params)
+
+    if @work.save
+      redirect_to works_path
+    else
+      render :new, status: :bad_request
+    end 
+  end
+
+
+
+
+  def work_params
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+  end
 end
