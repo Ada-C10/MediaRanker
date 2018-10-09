fe609935f6631181df5297e86006e3d9bac914b8
Trang <trang.frego@gmail.com>
2018-10-08 20:00:22 -0700

    create users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
   root 'welcome#index'
 
   resources :works
+  resources :users, only: [:index, :show, :new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
