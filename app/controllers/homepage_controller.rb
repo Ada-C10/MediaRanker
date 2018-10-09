8b0018860ddd7af991a9fb679655ce215d680c14
BarbaraWidjono <barbarawidjono@gmail.com>
2018-10-08 21:43:04 -0700

    Created Homepage#index instance variables for movies, albums and books

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,2 +1,7 @@
 class HomepageController < ApplicationController
+  def index
+    @movies = Work.where(category: "movie")
+    @books = Work.where(category: "book")
+    @albums = Work.where(category: "album")
+  end
 end
