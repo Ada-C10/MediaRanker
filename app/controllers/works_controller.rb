c7e499b9e83c3ffbcc73180c362606eee76982b7
BarbaraWidjono <barbarawidjono@gmail.com>
2018-10-09 20:00:46 -0700

    Added Flash messages to Work#create and view template

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,4 +2,31 @@ class WorksController < ApplicationController
   def index
     @works = Work.all
   end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    @work = Work.new(work_params)
+
+    if @work.save
+      flash[:success] = "#{@work.title} added sucessfully"
+      redirect_to works_path
+    else
+      flash.now[:error] = "Invalid media entry"
+      render :new, status: :bad_request
+    end
+  end
+
+  private
+  def work_params
+    return params.require(:work).permit(
+      :category,
+      :title,
+      :creator,
+      :publication_year,
+      :description
+    )
+  end
 end
