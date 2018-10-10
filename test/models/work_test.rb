2d8772594f1d1d89f0e275801c64d6738225a3d7
Leanne Rivera <rivera.leanne+ada@gmail.com>
2018-10-10 14:07:35 -0700

    model tests, fixtures done

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -6,4 +6,31 @@ describe Work do
   it "must be valid" do
     value(work).must_be :valid?
   end
+
+  it 'has required fields' do
+  fields = [:title, :creator, :description, :year, :category]
+
+  fields.each do |field|
+    expect(work).must_respond_to field
+  end
+end
+
+describe 'validations' do
+  it 'must have a title' do
+    # Arrange
+    work = works(:alanis)
+    work.title = nil
+
+    # Act
+    #valid = book.valid?
+    valid = work.save
+
+    # Assert
+    expect(valid).must_equal false
+    expect(work.errors.messages).must_include :title
+    expect(work.errors.messages[:title]).must_equal ["can't be blank"]
+  end
+
+
+
 end
