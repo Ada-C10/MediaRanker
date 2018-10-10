0cc92935601f3fa4915eaf248bb4e8321470c846
Katricia Smith <smithkatricia@gmail.com>
2018-10-09 18:31:31 -0700

    Correct validation syntax in work.rb

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,5 +1,5 @@
 class Work < ApplicationRecord
   has_many :votes
-  validates: title, presence: true
-  validates: category, presence: true
+  validates :title, presence: true
+  validates :category, presence: true
 end
