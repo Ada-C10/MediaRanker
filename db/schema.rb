2c21e8e96a42f0d1d8c03c8d371ed91f9406408c
Lindsay Terchin <lterchin@gmail.com>
2018-10-10 14:32:21 -0700

    changed data type of publication_year

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_08_232934) do
+ActiveRecord::Schema.define(version: 2018_10_10_213021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 2018_10_08_232934) do
   create_table "works", force: :cascade do |t|
     t.string "category"
     t.string "title"
-    t.date "publication_year"
+    t.string "publication_year"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
