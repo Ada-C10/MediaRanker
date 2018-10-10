86a5b824c813b52d36ad8e8f52d03617faba5347
Kay <kayxn23@gmail.com>
2018-10-10 13:47:38 -0700

    created update and destory methods to controller

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -30,6 +30,27 @@ class WorksController < ApplicationController
     end
   end
 
+  def update
+    @work = Work.find_by(id: params[:id].to_i)
+    if @work.update(work_params)
+      redirect_to work_path(@work.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    work = Work.find_by(id: params[:id].to_i)
+    if work.nil?
+      flash[:error] = "Work #{params[:id]} not found"
+    else
+      @deleted_work = work.destroy
+      flash[:success] = "#{work.title} deleted"
+    end
+
+    redirect_to root_path
+  end
+
   private
   def work_params
     return params.require(:work).permit(:id, :title, :creator, :description, :category, :publication_year)
