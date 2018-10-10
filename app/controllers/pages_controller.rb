f5d57a262126c03e73a6c2b343e6f438b118542c
Hayden Williams <hwilliams.deva@gmail.com>
2018-10-10 15:49:31 -0700

    get media spotlight functioning on homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,10 @@
 class PagesController < ApplicationController
   def show
-    @media = Medium.all
+    if params[:page] == "home"
+      @media = Medium.all
+      @media_spotlight = Medium.spotlight
+    end
+
     render template: "pages/#{params[:page]}"
   end
 end
