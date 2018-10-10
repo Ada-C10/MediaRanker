9da24ce3cb5d9957c4a6d705a4953d6b6504f901
jackie <jacquelyn.cheng@me.com>
2018-10-09 22:58:48 -0700

    Removed unnecessary user controller methods and routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,30 +16,4 @@ class UsersController < ApplicationController
       render :notfound, status: :not_found
     end
   end
-
-  # TODO: delete these methods and associations
-  # def new
-  #   @current_user = User.find_by(id: session[:user_id])
-  #   # user id will be nil if not logged in or whole author model if they are logged in
-  #
-  #   @user = User.new
-  # end
-
-  # def create
-  #   @user = User.new(user_params)
-  #   @user.joined = Date.today
-  #   raise
-  #
-  #   if @user.save
-  #     redirect_to user_path
-  #   else
-  #     render :new
-  #   end
-  # end
-
-  private
-
-    def user_params
-      return params.require(:user).permit(:username)
-    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   resources :works
   post 'works/:id/upvote', to: 'works#upvote', as: 'upvote'
 
-  resources :users, only: [:index, :show, :new, :create]
+  resources :users, only: [:index, :show]
 end
