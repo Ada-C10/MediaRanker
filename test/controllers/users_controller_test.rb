d9144c670e516d37b3b0a52276e589d5a68f8327
jackie <jacquelyn.cheng@me.com>
2018-10-09 23:02:25 -0700

    Forgot to save this file in the previous commit

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -10,15 +10,4 @@ describe UsersController do
     get users_show_url
     value(response).must_be :success?
   end
-
-  it "should get new" do
-    get users_new_url
-    value(response).must_be :success?
-  end
-
-  it "should get create" do
-    get users_create_url
-    value(response).must_be :success?
-  end
-
 end
