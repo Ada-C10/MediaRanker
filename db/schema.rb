bf5d2b7f3552df1aee4f3d773750f042a077c7f2
Dionisia Montanez <dmontanez15@gmail.com>
2018-10-09 20:16:22 -0700

    added foreign keys to vote.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_09_002148) do
+ActiveRecord::Schema.define(version: 2018_10_10_031428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,10 +22,12 @@ ActiveRecord::Schema.define(version: 2018_10_09_002148) do
   end
 
   create_table "votes", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "work_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.bigint "work_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
+    t.index ["work_id"], name: "index_votes_on_work_id"
   end
 
   create_table "works", force: :cascade do |t|
@@ -38,4 +40,6 @@ ActiveRecord::Schema.define(version: 2018_10_09_002148) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "votes", "users"
+  add_foreign_key "votes", "works"
 end
