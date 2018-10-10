b2c2cadbad72662d4dcf3f0fc2136a839c4ce357
Hannah <hannahvanseeters@gmail.com>
2018-10-10 10:55:01 -0700

    added logged in as button as well as changed @current_user to application controller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,4 +1,17 @@
 class VotesController < ApplicationController
   def index
   end
+  # need to add upvote as a column
+  def upvote
+    @vote = Vote.new
+    # += 1?
+    @vote.quantity = 1
+    @vote.user = params[:user_id]
+    @vote.work = params[:work_id]
+    if @vote.save
+      redirect_back fallback_location: works_path
+    else
+      puts "#{@vote.errors}"
+    end
+  end
 end
