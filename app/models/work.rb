d9835085bdaa40923df6ecbf0259c2f2fd77ec09
Goeun Park <goeuntothepark@gmail.com>
2018-10-09 17:49:36 -0700

    add validations

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,9 @@
 class Work < ApplicationRecord
+  has_many :votes, dependent: :nullify
+
+  validates :title uniqueness: true
+  validates :creator, presence: true
+  validates :category, presence: true
+  validates :pub_year, numericality: {only_integer: true, less_than_or_equal_to: 2018}
+  validates
 end
