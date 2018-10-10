eda99edaa1c9db315d8a8bc302b2e848e184af40
jackie <jacquelyn.cheng@me.com>
2018-10-10 09:39:33 -0700

    Added relationship tests for vote model -- passing tests

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,9 +1,39 @@
 require "test_helper"
 
 describe Vote do
-  let(:vote) { Vote.new }
+  let(:vote) { votes(:vote_one) }
 
   it "must be valid" do
     value(vote).must_be :valid?
+  end
+
+  it 'has required fields' do
+    fields = [:date, :work, :user]
+
+    fields.each do |field|
+      expect(vote).must_respond_to field
+    end
+  end
+
+  describe 'relationships' do
+    it 'belongs to a user' do
+      # Arrange is done with let
+
+      # Act
+      user = vote.user
+
+      # Assert
+      expect(user).must_be_instance_of User
+    end
+
+    it 'belongs to a work' do
+      # Arrange is done with let
+
+      # Act
+      work = vote.work
+
+      # Assert
+      expect(work).must_be_instance_of Work
+    end
   end
 end
