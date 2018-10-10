b93403f066236c570f6ccf734fd388ccfe3f19e3
BarbaraWidjono <barbarawidjono@gmail.com>
2018-10-10 15:37:22 -0700

    Create login method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    name = params[:username]
+    user = User.find_by(username: name)
+
+    if user
+      flash[:success] = "Successfully logged in as #{user.username}"
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      new_user = User.new(username: name)
+      new_user.save
+      flash[:success] = "Successfully created a new user #{new_user.username} with id #{new_user.id}"
+      session[:user_id] = new_user.id
+      redirect_to root_path
+    end
+  end
 end
