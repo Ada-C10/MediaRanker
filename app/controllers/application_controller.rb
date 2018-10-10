c8c005d6dcc6fc2b05a2457edf0a3e7d0aa2b932
Aurea <fu.sakura@gmail.com>
2018-10-10 14:48:58 -0700

    finish login logout functionality on header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+  before_action :find_logged_in_user
+
+  private
+
+  def find_logged_in_user
+    @user = User.find_by(id: session[:user_id])
+  end
 end
