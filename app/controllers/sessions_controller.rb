8529ff219645647e3a672ca9a45340b76f2a1ae0
kangazoom <michellelcronin@gmail.com>
2018-10-09 22:21:31 -0700

    updated sessions controller - all methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,28 +1,27 @@
 class SessionsController < ApplicationController
   def new
-    # QUESTION: PUT OBJ HERE? NO B/C SESSIONS NOT AN OBJ?
+    @user = User.new()
   end
 
-def create
-  # QUESTION: or def login???
-	username = params[:username]
-	user = User.find_by(username: username)
+  def create
+    username = params[:username]
+    @user = User.find_by(username: username) #QUESTION: param obj or attribute???
 
-	if user
-		flash[:success] = "Successfuly logged in as existing user #{name}"
-	else
-    user = User.new(username: username) # if user not found, create a new one
-    # QUESTION: create if/else if not saved successfully??
-		flash.now[:success] = "Successfully created new user #{username} with ID #{user.id}"
-	end
-  
-  session[:user_id] = user.id
-  redirect_to root_path
-end
+    if @user
+      flash[:success] = "Successfuly logged in as existing user #{username}"
+    else
+      @user = User.new(username: username) # if user not found, create a new one
+      # QUESTION: create if/else if not saved successfully??
+      flash[:success] = "Successfully created new user #{username} with ID #{user.id}"
+    end
 
-def logout
-	session[:user_id] = nil
-	flash[:success] = "Successfully logged out"
-	redirect_to root_path
-end
+    session[:user_id] = @user.id
+    redirect_to root_path
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "Successfully logged out"
+    redirect_to root_path
+  end
 end
