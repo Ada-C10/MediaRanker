93a43c774419551862228f7bfba33dac4f635094
Valerie Gidzinski <valgidzi@gmail.com>
2018-10-09 20:36:20 -0700

    Create partial for top lists on main page, add media spotlight section

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def index
+    @works = Work.all
+    @books = Work.where(category: 'book').first(10)
+    @albums = Work.where(category: 'album').first(10)
+    @movies = Work.where(category: 'movie').first(10)
   end
 end
