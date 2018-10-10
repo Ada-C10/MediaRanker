564275836be51fcb8a2638fb32f2795c8f434750
KatherineJF <kjfitzpat@hotmail.com>
2018-10-09 19:01:47 -0700

    user can login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,17 @@
 class SessionsController < ApplicationController
   def login
+    user = User.find_by(name: params[:user][:name])
+    if user.nil?
+      user = User.create(name: params[:user][:name])
+    end
+    session[:user_id] = user.id
+    flash[:success] = "#{user.name} Successfully logged in!"
+    redirect_to root_path
   end
 
+
   def new
+    @user = User.new
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   root "main#index"
   resources :works
   resources :main, only: [:index]
+  post 'sessions/login', to: 'sessions#login', as: 'login'
+  get 'sessions/login', to: 'sessions#new'
+  delete 'sessions/destroy', to: 'sessions#destroy', as: 'logout'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
