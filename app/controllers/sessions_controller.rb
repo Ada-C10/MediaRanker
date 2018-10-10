4b273307689c481c35b9ce0a58f53cfe076cdc83
Karis Kim <ouniiq@hotmail.com>
2018-10-10 16:31:06 -0700

    made users to create new username or log in with existing username

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
 class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    name = params[:username]
+    user = User.find_by(username: name)
+
+    if user
+      flash[:success] = "Successfully logged in as #{name}"
+      session[:user_id] = user.id
+      redirect_to home_path
+    else
+      @user = User.new(username: name)
+      if @user.save
+        redirect_to home_path
+      else
+        render :new
+      end
+    end
+  end
 end
