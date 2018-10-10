fa0c8fa22beaddc6a0d4bac9e9abd445051e1385
jackie <jacquelyn.cheng@me.com>
2018-10-09 20:26:33 -0700

    Fixed typo in Users#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     @current_user = User.find_by(id: session[:user_id])
     # user id will be nil if not logged in or whole author model if they are logged in
 
-    @users = User.find_by(id: params[:id].to_i)
+    @user = User.find_by(id: params[:id].to_i)
 
     if @user.nil?
       render :notfound, status: :not_found
