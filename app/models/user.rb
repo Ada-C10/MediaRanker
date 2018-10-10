91762c95f0765a06d815c330d2b84d47687ea3d5
Sigrid Benezra <sdbenezra@gmail.com>
2018-10-10 14:49:25 -0700

    Add basic user controller methods, views, and validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+
+  # relation - user can have many votes
+  has_many :votes
+  # validate name exists and is unique
+  validates :name, presence: true, uniqueness: true
 end
