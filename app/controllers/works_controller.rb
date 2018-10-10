92267456f55785a7b6fb3b08dfaa18f97be234f4
Laura <larmartinez2@gmail.com>
2018-10-09 22:24:21 -0700

    list all media

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,5 +1,6 @@
 class WorksController < ApplicationController
   def index
+    @work = Work.all 
   end
 
   def delete
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
-  get 'works/index'
-  get 'works/delete'
-  get 'works/edit'
-  get 'works/show'
-  get 'works/update'
-  get 'works/upvote'
-  root 'pages#index'
+  # get 'works/index'
+  # get 'works/delete'
+  # get 'works/edit'
+  # get 'works/show'
+  # get 'works/update'
+  # get 'works/upvote'
+  # root 'pages#index'
   # get 'pages/index'
   post 'sessions/login', to: 'sessions#login', as: 'login'
   get 'sessions/login', to: 'sessions#new'
   delete 'sessions/destroy', to: 'sessions#destroy', as: 'logout'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :works
+  delete '/works/:id', to: 'works#destroy', as: 'destroy'
 end
