e8ac504fa5993dd7c2e5b8e6a55368b372dd98b4
Valerie Gidzinski <valgidzi@gmail.com>
2018-10-09 19:26:52 -0700

    Separate works by category in WorksController#index

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,7 +1,9 @@
 class WorksController < ApplicationController
 
   def index
-    @works = Work.all
+    @books = Work.where(category: 'book')
+    @albums = Work.where(category: 'album')
+    @movies = Work.where(category: 'movie')
   end
 
   def show
