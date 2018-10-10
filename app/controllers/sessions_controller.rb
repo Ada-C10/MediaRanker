595803181fb77a43f72d46e1b7582f6e42083c48
Amber Lynn <griffiam@gmail.com>
2018-10-10 15:56:23 -0700

    updated actions in works and sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,23 @@
 class SessionsController < ApplicationController
-  def new
-  end
 
   def create
-    # username = params[:username]
-    # user = User.find_by(username: username)
-    #
-    # if user
-    #   flash[:success] = "Successfully logged in as #{username}"
-    #   session[:user_id] = user.id
-    #   redirect_to user_path(user)
-    # else
-    #   flash.now[:error] = "No such user \"#{username}\" "
-    #   render :new
-    # end
+    username = params[:user_name]
+    user = User.find_by(username: username)
+
+    if user
+      flash[:success] = "Successfully logged in as #{username}"
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+
+    else
+      flash.now[:error] = "No such username \"#{username}\""
+      render :new
+    end
+  end
+
+  def logout
+    session[:user_id] = nil
+    flash[:success] = "Successfully logged out"
+    redirect_to root_path
   end
 end
--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,14 +1,94 @@
 class WorksController < ApplicationController
   def index
-    @works = Work.all
+    if params[:user_id]
+      user = User.find_by(id: params[:user_id])
+      # How do we handle if the user is not found?
+      #  If the user is not found, we would have a conditional to check
+      # that the user was found.
+      # Redirect to a 404 page
+      # Redirect to another existing page (the home page?) (new user form?)
+      # Show an error message saying that the user was not found
+      @works = user.works
+    else
+      @works = Work.all
+    end
   end
 
-
   def show
-    work_id = params[:id]
-    @work = Work.find_by(id: work_id)
     if @work.nil?
       head :not_found
     end
+  end
+
+  def new
+    if params[:user_id]
+      user = User.find_by(id: params[:user_id])
+      @work = user.works.new
+      # Same thing as above:
+      # @work = Work.new(user: user)
+    else
+      @work = Work.new
+    end
+  end
+
+  def create
+    filtered_work_params = work_params()
+    @work = Work.new(filtered_work_params)
+
+    is_successful_save = @work.save
+
+    if is_successful_save
+      flash[:success] = "Successfully created new work with title \"#{@work.title}\""
+      redirect_to works_path
+    else
+      flash.now[:error] = "Invalid work data"
+      render :new, status: :bad_request
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @work.update(work_params)
+      flash[:success] = "Successfully updated work \"#{@work.title}\""
+      redirect_to work_path(@work.id)
+    else
+      flash.now[:error] = "Invalid work data"
+      render(:edit, status: :bad_request)
+    end
+  end
+
+  def destroy
+    if @work.user_id == session[:user_id]
+      @work.destroy
+
+      flash[:success] = "Successfully destroyed work \"#{@work.title}\""
+      redirect_to works_path
+
+    else
+      flash[:error] = "You must be logged in as a work's user in order to delete it!"
+
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+
+  private
+
+  # Strong params: only let certain attributes
+  # through
+  def work_params
+    return params.require(:work).permit(
+      :title,
+      :publication_year,
+      :description,
+      :creator,
+      :category
+    )
+  end
+
+  def find_work
+    @work = Work.find_by(id: params[:id])
   end
 end
