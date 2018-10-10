7d326f61d2c5b8f8f86d0e1d048126f4274721c7
Melissa O'Hearn <melissa.ohearn@gmail.com>
2018-10-10 14:25:15 -0700

    add show method for works

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -31,6 +31,13 @@ class WorksController < ApplicationController
     end
   end
 
+  def edit
+    @work = Work.find_by(id: params[:id])
+    if @work.nil?
+      head :not_found
+    end
+  end
+
   private
 
     def work_params
