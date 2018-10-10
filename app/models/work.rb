769ded5df85aec399a6cade4c492f68055461f76
Katricia Smith <smithkatricia@gmail.com>
2018-10-09 21:22:37 -0700

    Add validation for category names to work model. Add validation to title uniqueness

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,5 +1,8 @@
 class Work < ApplicationRecord
+
+  WORK_CATEGORIES = %w(album movie book)
+
   has_many :votes
-  validates :title, presence: true
-  validates :category, presence: true
+  validates :title, presence: true, uniqueness: true
+  validates :category, presence: true, inclusion: { in: WORK_CATEGORIES}
 end
