b53031d91dd2e3ec97251f785f3054a2f15c7606
xLJohnsonx <lizjohnsonemail@gmail.com>
2018-10-09 22:35:52 -0700

    votes routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,6 @@ Rails.application.routes.draw do
   delete 'sessions/destroy', to: 'sessions#destroy', as: 'logout'
 
   resources :works
-  # get 'works/index'
-  # get 'works/show'
-  # get 'works/update'
-  # get 'works/create'
-  # get 'works/new'
-  # get 'works/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
