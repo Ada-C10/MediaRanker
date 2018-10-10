019eb8e56badd39616a5716d423a26f999f2cefd
kangazoom <michellelcronin@gmail.com>
2018-10-09 21:52:33 -0700

    ugh fixed routing i think ugh

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 
-  resources :works, :users, :votes
+  resources :users, only: [:index, :show, :create]
 
-  # QUESTION:  no delete for users? no delete for votes?
-  # nested routes for works--votes?
+  resources :works do
+    resources :votes, only: [:create]
+  end
 
-  resources :sessions, only: [:new, :create, :delete]
-
-  post '/sessions/logout', to: 'sessions#logout', as: 'logout'
+  get '/login', to: 'sessions#new', as: 'login_form'
+  post '/login', to: 'sesstions#create' as: 'login'
+  delete '/login', to: 'sessions#destroy', as: 'logout'
 
 end
