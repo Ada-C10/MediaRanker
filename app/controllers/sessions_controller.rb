7af60c8ce520875f05032937c028376344fe893b
Amber Lynn <griffiam@gmail.com>
2018-10-10 14:01:40 -0700

    added erb to to works index and show page. successfully loaded seed file

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,15 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-    username = params[:username]
-    user = User.find_by(username: username)
-
-    if user
-      flash[:success] = "Successfully logged in as #{username}"
-      session[:user_id] = user.id
-      redirect_to user_path(user)
-    else
-      flash.now[:error] = "No such user \"#{username}\" "
-      render :new
+    # username = params[:username]
+    # user = User.find_by(username: username)
+    #
+    # if user
+    #   flash[:success] = "Successfully logged in as #{username}"
+    #   session[:user_id] = user.id
+    #   redirect_to user_path(user)
+    # else
+    #   flash.now[:error] = "No such user \"#{username}\" "
+    #   render :new
+    # end
   end
 end
