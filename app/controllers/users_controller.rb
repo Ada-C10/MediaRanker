6e5e3fed2486c486e3f6683ab5cdb21025fce3e6
Karis Kim <ouniiq@hotmail.com>
2018-10-10 05:26:17 -0700

    allowed user to add new username and made it show to all users list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,7 +46,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    return params.require(:work).permit(
+    return params.require(:user).permit(
       :username
     )
   end
