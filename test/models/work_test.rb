97ef2d9aaad87d43977ef391d9185a80b0c8a388
Carly Reiter <yepodacarly@gmail.com>
2018-10-09 17:30:16 -0700

    Added validation testing of the work model.

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,9 +1,51 @@
 require "test_helper"
 
 describe Work do
-  let(:work) { Work.new }
+  describe 'validations' do
+    before do
+      @work = Work.new(
+        title: 'Beanstock',
+        description: 'this is a test',
+        year_published: 1999,
+        #this creator will be replaced by User
+        created_by: 'Carly',
+        category: 'Movie'
+      )
+    end
 
-  it "must be valid" do
-    value(work).must_be :valid?
+    it 'is valid when title, creator, and category are present' do
+
+      is_valid = @work.valid?
+      expect(is_valid ).must_equal true
+
+    end
+
+    it 'is invalid without a title' do
+      @work.title = nil
+
+      is_valid = @work.valid?
+
+      expect( is_valid ).must_equal false
+      expect( @work.errors.messages ).must_include :title
+    end
+
+    it 'is invalid without a creator' do
+      @work.created_by = nil
+
+      is_valid = @work.valid?
+
+      expect( is_valid ).must_equal false
+      expect( @work.errors.messages ).must_include :created_by
+    end
+
+    it 'is invalid without a category' do
+      @work.category = nil
+
+      is_valid = @work.valid?
+
+      expect( is_valid ).must_equal false
+      expect( @work.errors.messages ).must_include :category
+    end
+
   end
 end
