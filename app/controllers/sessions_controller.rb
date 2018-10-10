a7a643441af94242d768728696ef828acbfbf1bb
BarbaraWidjono <barbarawidjono@gmail.com>
2018-10-10 16:15:31 -0700

    Login and logout functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,4 +18,10 @@ class SessionsController < ApplicationController
       redirect_to root_path
     end
   end
+
+  def logout
+    session[:user_id] = nil
+    flash[:success] = "Successfully logged out"
+    redirect_to root_path
+  end
 end
