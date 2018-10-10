78b43bc0e1727cf0da74a77d304df68c20152efc
Laura <larmartinez2@gmail.com>
2018-10-09 23:13:56 -0700

    create edit method and view page

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -7,8 +7,20 @@ class WorksController < ApplicationController
   end
 
   def edit
+    @work = Work.find(params[:id].to_i)
   end
 
+  # does not update
+  def update
+    @work = Work.find_by(id: params[:id].to_i)
+    if @work.update(work_params)
+      redirect_to work_path(@work.id)
+    else
+      render :edit
+    end
+  end
+
+  # does
   def show
     id = params[:id].to_i
     @work = Work.find_by(id: id)
@@ -17,9 +29,24 @@ class WorksController < ApplicationController
     end
   end
 
-  def update
+  def new
+    @work = Work.new
   end
 
+  # def create
+  #   @work = Work.new(work_params)
+  #   if @work.save
+  #     redirect_to work_path
+  #   else
+  #     render :new
+  #   end
+  # end
+
   def upvote
+  end
+
+  private
+  def work_params
+    return params.require(:work).permit(:title, :category, :creator, :publication_year, :description)
   end
 end
