b59298b9913b781fc334ddd4d16fb0bdcccc84d8
kangazoom <michellelcronin@gmail.com>
2018-10-09 22:25:30 -0700

    updated users controller (index+show methods)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,63 +2,17 @@ class UsersController < ApplicationController
 
     def index
       # @user = User.all.order(:due_date)
-        @users = User.allorder(:created_at)
+        @users = User.all.order(:created_at)
     end
 
-    def show # TODO: add flash etc???
-      if @user = User.find_by(id: params[:id].to_i)
+    def show
+      @user = User.find_by(id: params[:id])
+      if @user
+        return @user
       else
-        return head :not_found
+        return head :not_found #QUESTION: status?
       end
     end
-
-    # def new
-    #   @user = User.new
-    # end
-    #
-    # def create
-    #   @user = User.new(user_params)
-    #
-    #   result = @user.save
-    #
-    #   if result
-    #     redirect_to users_path
-    #   else
-    #     render :new
-    #   end
-    # end
-    #
-    # def edit
-    #   @user = User.find_by(id: params[:id])
-    #   if !@user
-    #     return head :not_found
-    #   end
-    # end
-    #
-    # def update
-    #   @user = User.find(params[:id])
-    #
-    #   result = @user.update(user_params)
-    #
-    #   if result
-    #     redirect_to user_path(@user)
-    #   else
-    #     render :edit
-    #   end
-    # end
-    #
-    # def destroy
-    #
-    #   @user = User.find_by(id: params[:id])
-    #
-    #   if @user
-    #     @user.destroy
-    #     redirect_to users_path
-    #   else
-    #     return head :not_found
-    #   end
-    # end
-
 
     private
 
