8fcb61464daa0044061e84f1d83471b424a7256e
BarbaraWidjono <barbarawidjono@gmail.com>
2018-10-10 15:14:52 -0700

    Created a Sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homepage#index'
-  resources :works, :users
+  resources :works, :users, :votes
+
+  resources :sessions, only: [:new, :create]
+  post '/sessions/logout', to: 'sessions#logout', as: 'logout'
 end
