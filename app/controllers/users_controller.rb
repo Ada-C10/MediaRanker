ad4839e56be3a40729d58ee641c1390d2d442053
Pushpa Agr <pushpaagr108@gmail.com>
2018-10-10 17:36:10 -0700

    added method in applications controller to find_user_id for the session, reeditted in the view template for the layout application template to reflect that change

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,22 +3,27 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-def create
-  @user = User.new(user_params)
-  if @user.save
-    redirect_to root_path
-  else
-    render :new
+
+  def index
+    @users = User.all
   end
-end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
 
 
-private
+  private
 
-def user_params
-  return params.require(:username).permit(:username)
-end
+  def user_params
+    return params.require(:username).permit(:username)
+  end
 
 
 end
