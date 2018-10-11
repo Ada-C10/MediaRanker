6bf0ecc7abf38645327297eb314e7d5c1ff81938
Dionisia Montanez <dmontanez15@gmail.com>
2018-10-10 19:42:05 -0700

    added vote counter to users and works.

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,6 +1,7 @@
 class Work < ApplicationRecord
   has_many :votes
+  has_many :users, through: :votes 
   validates :title, presence: true, uniqueness: true
   validates :category, presence: true, inclusion: { in: %w(album movie book) }
-  validates :publication_year, numericality: { only_integer: true } 
+  validates :publication_year, numericality: { only_integer: true }
 end
