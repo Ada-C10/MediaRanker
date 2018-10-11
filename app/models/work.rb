598430595688ef0e15d415bc09433123d44d60a1
Goeun Park <goeuntothepark@gmail.com>
2018-10-10 22:47:01 -0700

    forgot comma

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,7 +1,7 @@
 class Work < ApplicationRecord
   has_many :votes, dependent: :nullify
 
-  validates :title uniqueness: true
+  validates :title, uniqueness: true
   validates :creator, presence: true
   validates :category, presence: true
   validates :pub_year, numericality: {only_integer: true, less_than_or_equal_to: 2018}
