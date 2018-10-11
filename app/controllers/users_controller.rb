c80536bbef168eef8efd0d53946a94217a71f404
jfahmy <jacqueline.fahmy0.o@gmail.com>
2018-10-10 18:06:14 -0700

    add user show and index view, fix table on work views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,20 @@
 class UsersController < ApplicationController
+  before_action :get_user, only: [:show]
 
   def index
     @users = User.all
+  end
+
+  def show; end
+
+  private
+
+  def get_user
+    @user = User.find(params[:id])
+
+    if @user.nil?
+      flash.now[:warning] = "Cannot find the user"
+    end
   end
 
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,4 +3,8 @@ class Vote < ApplicationRecord
   belongs_to :user
 
   validates :work_id, uniqueness: {scope: :user_id, message: "You have already voted for this work."}
+
+  def get_work
+    @work = Work.find(self.work_id)
+  end
 end
