5bacc7e2b5e8995c580e335e73c4d00bc994e470
Lindsay Terchin <lterchin@gmail.com>
2018-10-10 18:14:36 -0700

    fixed edit work functionality

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -24,6 +24,18 @@ class WorksController < ApplicationController
     end
   end
 
+  def edit
+    @work = Work.find_by(id: params[:id])
+  end
+
+  def update
+    @work = Work.find_by(id: params[:id])
+
+    if @work.update(work_params)
+      redirect_to work_path(@work)
+    end
+  end
+
 
   private
   def work_params
