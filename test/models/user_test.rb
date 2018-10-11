586ab7a026fd1110cf0393a64d325e5e8df71671
Amber Lynn <griffiam@gmail.com>
2018-10-11 16:37:50 -0700

    completed validation tests for work model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -7,33 +7,36 @@ describe User do
     expect(user).must_be :valid?
   end
 
-  it "won't be valid without username" do
-    user = users(:one)
-    user.username = nil
+  describe "validations" do
+    it "won't be valid without username" do
+      user = users(:one)
+      user.username = nil
 
-    result = user.valid?
+      result = user.valid?
 
-    expect(result).wont_equal true
-  end
+      expect(result).wont_equal true
+    end
 
-  it "will be valid with a username" do
-    result = user.valid?
-    expect(result).must_equal true
-  end
+    it "will be valid with a username" do
+      result = user.valid?
+      expect(result).must_equal true
+    end
 
-  it "won't be valid without unique username" do
-    new_user = User.new(username: "griffiam")
+    it "won't be valid without unique username" do
+      new_user = User.new(username: "griffiam")
 
-    result = new_user.valid?
+      result = new_user.valid?
 
-    expect(result).wont_equal true
-  end
+      expect(result).wont_equal true
+    end
 
-  it "will be valid with unique username" do
-    newest_user = User.new(username: "guru")
+    it "will be valid with unique username" do
+      newest_user = User.new(username: "guru")
 
-    new_result = newest_user.valid?
+      new_result = newest_user.valid?
 
-    expect(new_result).must_equal true
+      expect(new_result).must_equal true
+    end
+
   end
 end
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,11 +1,85 @@
 require "test_helper"
 
 describe Work do
-  # let(:work) { Work.new }
-  #
-  # it "must be valid" do
-  #   value(work).must_be :valid?
-  # end
+  let(:work) { works(:one) }
 
-  
+  it "must be valid" do
+    expect(work).must_be :valid?
+  end
+
+
+  describe "validations" do
+    it "won't be valid without title" do
+      work = works(:one)
+      work.title = nil
+
+      result = work.valid?
+
+      expect(result).wont_equal true
+    end
+
+    it "will be valid with a title" do
+      result = work.valid?
+      expect(result).must_equal true
+    end
+
+    it "won't be valid without unique title" do
+      new_work = Work.new(title: Work.first.title)
+
+      result = new_work.valid?
+
+      expect(result).wont_equal true
+    end
+
+    it "will be valid with unique title" do
+      new_work = Work.new(title: "workworkworkworkwork", category: "movie", creator: "memyselfandI", publication_year: 1, description: "test-description")
+
+      new_result = new_work.valid?
+
+      expect(new_result).must_equal true
+    end
+
+    it "will only be valid with correct category" do
+      work = works(:one)
+
+      result = work.valid?
+      expect(result).must_equal true
+    end
+
+    it "will not be valid without correct category" do
+      work = works(:one)
+      work.category = "film-reel"
+
+      result = work.valid?
+      expect(result).wont_equal true
+    end
+
+    it "will not be valid without a publication year" do
+      work = works(:one)
+      work.publication_year = nil
+
+      result = work.valid?
+
+      expect(result).wont_equal true
+    end
+
+    it "will not be valid without creator present" do
+      work = works(:one)
+      work.creator = nil
+
+      result = work.valid?
+
+      expect(result).wont_equal true
+    end
+
+    it "will not be valid without description present" do
+      work = works(:one)
+      work.description = nil
+
+      result = work.valid?
+
+      expect(result).wont_equal true
+    end
+  end
+
 end
