269e3f7c70ef0435dbdf06c3e6c8b0c9e4532976
Amber Lynn <griffiam@gmail.com>
2018-10-10 20:07:00 -0700

    updated work form partials and work new view

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,5 +1,8 @@
 class Work < ApplicationRecord
   has_many :votes
 
+  CATEGORIES = %w(movie book album)
+
   validates :title, presence: true, uniqueness: true
+  validates :category, inclusion: { in: CATEGORIES }
 end
