2b6268161255e79f42ab1cc3e64108d2da0c0ccb
Amanda Ungco <amanda.ungco@gmail.com>
2018-10-10 18:33:30 -0700

    added user/votes models and added references to works

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_09_231757) do
+ActiveRecord::Schema.define(version: 2018_10_11_013240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "works", force: :cascade do |t|
     t.string "title"
@@ -23,6 +34,12 @@ ActiveRecord::Schema.define(version: 2018_10_09_231757) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.bigint "vote_id"
+    t.index ["user_id"], name: "index_works_on_user_id"
+    t.index ["vote_id"], name: "index_works_on_vote_id"
   end
 
+  add_foreign_key "works", "users"
+  add_foreign_key "works", "votes"
 end
