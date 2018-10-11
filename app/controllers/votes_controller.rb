041b618a6f231144dbff7699b8ff2b63229b6255
Daniela Di Iorio <danielad@gmail.com>
2018-10-10 20:58:45 -0700

    dates formatted correctly in views

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,6 +4,7 @@ class VotesController < ApplicationController
       flash[:warning] = "A problem occurred: You must log in to do that"
     else
       Vote.create(work_id: params[:id], user_id: session[:user_id])
+      flash[:success] = "Successfully upvoted!"
     end
     redirect_back fallback_location: root_path
   end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,4 +2,9 @@ class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :work
   validates :user, uniqueness: { scope: [:work], message: 'has already voted for this work'}
+
+def upvote_date
+  return self.created_at.strftime("%b %d, %Y")
+end
+
 end
