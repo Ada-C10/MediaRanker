3813a2a065619ec2a0915ae9bc8740da87dbd47d
Anibel America <ameriabel@gmail.com>
2018-10-10 20:48:57 -0700

    adds tests for Vote get_date method

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -31,12 +31,38 @@ describe Vote do
         user_id: Vote.first.user_id,
         work_id: Vote.first.work_id
       )
-      
+
       expect(new_vote.valid?).must_equal false
+    end
+  end
+
+  describe 'relations' do
+    before do
+      @vote = Vote.first
+    end
+
+    it 'belongs to a user' do
+      user = @vote.user
+
+      expect(user).must_be_instance_of User
+    end
+
+    it 'belongs to a work' do
+      work = @vote.work
+
+      expect(work).must_be_instance_of Work
+    end
+  end
+
+  describe 'Vote.get_date method' do
+    it 'returns a date formatted Mmm D, YYYY' do
+      date = Vote.get_date(Vote.first)
+
+      # regex for Mmm DD, YYYY , ex: Oct 10, 2010
+      matches_format = (/[A-Z][a-z]{2}\s\d{2},\s\d{1,4}/) =~ date
+
+      expect(matches_format != nil).must_equal true
     end
 
   end
-
-
-
 end
