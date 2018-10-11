1483b7c21666c2e6b08a46264fed8885231c91db
BarbaraWidjono <barbarawidjono@gmail.com>
2018-10-11 11:21:59 -0700

    Any logged in user can upvote a work

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,19 +1,24 @@
 class VotesController < ApplicationController
+
+  def new
+    @vote = Vote.new
+  end
+
   def create
     session[:user_id] != nil ? user_logged_in = true : user_logged_in = false
 
     if user_logged_in
-      does_vote_exist = Vote.where(user_id: session[:user_id], work_id: params[:work_id])
+      # does_vote_exist = Vote.where(user_id: session[:user_id], work_id: params[:work_id])
 
-      if does_vote_exist
-        flash.now[:error] = "A problem occurred: Could not upvote"
-        redirect_to work_path(params[:work_id])
-      else
-        new_vote = Vote.new(user_id: session[:user_id], work_id: params[:work_id])
-        new_vote.save
+      # if does_vote_exist
+      #   flash.now[:error] = "A problem occurred: Could not upvote"
+      #   redirect_to work_path(params[:work_id])
+      # else
+        @vote = Vote.new(user_id: session[:user_id], work_id: params[:work_id])
+        @vote.save
         flash[:sucess] = "Successfully upvoted"
         redirect_to work_path(params[:work_id])
-      end
+      # end
     else
       flash[:error] = "You must be logged in to vote"
       redirect_to new_session_path
