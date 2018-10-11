0560ebe80d5aab3549887d71551e3d91a70c43fd
BarbaraWidjono <barbarawidjono@gmail.com>
2018-10-11 12:15:17 -0700

    Logic that a user can upvote any media once

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -8,17 +8,18 @@ class VotesController < ApplicationController
     session[:user_id] != nil ? user_logged_in = true : user_logged_in = false
 
     if user_logged_in
-      # does_vote_exist = Vote.where(user_id: session[:user_id], work_id: params[:work_id])
+      does_vote_exist = Vote.where(user_id: session[:user_id].to_s, work_id: params[:work_id].to_s)
 
-      # if does_vote_exist
-      #   flash.now[:error] = "A problem occurred: Could not upvote"
-      #   redirect_to work_path(params[:work_id])
-      # else
+      if does_vote_exist == []
         @vote = Vote.new(user_id: session[:user_id], work_id: params[:work_id])
         @vote.save
         flash[:sucess] = "Successfully upvoted"
         redirect_to work_path(params[:work_id])
-      # end
+
+      else
+        flash[:error] = "A problem occurred: Could not upvote. You've already upvoted this work."
+        redirect_to work_path(params[:work_id])
+      end
     else
       flash[:error] = "You must be logged in to vote"
       redirect_to new_session_path
