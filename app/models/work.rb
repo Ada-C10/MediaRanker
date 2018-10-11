e32768519e57539f5527f6b97a3e9ed70c060463
BarbaraWidjono <barbarawidjono@gmail.com>
2018-10-11 12:51:52 -0700

    Created Votes model methods: find_work_id, total_votes

--- app/models/work.rb
+++ app/models/work.rb
@@ -3,4 +3,17 @@ class Work < ApplicationRecord
 
   validates :title, presence: true, uniqueness: true
   validates :category, presence: true
+
+  def find_work_id
+    return self.id
+  end
+
+  def total_votes
+    work_id = find_work_id
+
+    array_of_votes = Vote.where(work_id: work_id)
+    num_of_votes = array_of_votes.count
+
+    return num_of_votes
+  end
 end
