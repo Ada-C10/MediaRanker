3832cee163bc049f1926c43e13a4be05f11cf8c5
Chantelle Belic <chantelle.belic@gmail.com>
2018-10-10 21:18:33 -0700

    added validations and passing tests for vote

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,9 @@
 class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :work
+
+  validates :user_id, presence: true
+  validates :work_id, presence: true
+  validates :work, uniqueness: {
+    scope: :user, message: "Sorry user, you can't vote for the same work twice." }
 end
