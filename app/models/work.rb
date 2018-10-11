9e43a0f8cb0d9926fce16ca62c080192f442d98c
Goeun Park <goeuntothepark@gmail.com>
2018-10-10 22:47:41 -0700

    typo

--- app/models/work.rb
+++ app/models/work.rb
@@ -5,5 +5,4 @@ class Work < ApplicationRecord
   validates :creator, presence: true
   validates :category, presence: true
   validates :pub_year, numericality: {only_integer: true, less_than_or_equal_to: 2018}
-  validates
 end
