ccac6bcee5150dd6ea9019b6a8e33b113ce5fbd6
Madaleine Shields <maddiev.shields@gmail.com>
2018-10-12 15:21:45 -0700

    Write login/logout methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,26 @@
 class SessionsController < ApplicationController
+
   def new
   end
 
   def create
+    name = params[:username]
+    user = User.find_by(username: name)
+
+    if user
+      flash[:success] = "Successfully logged in as #{name}"
+
+    else
+      user = User.create(username: name)
+      flash[:success] = "Successfully created new user #{user.name} with ID #{user.id}"
+    end
+    session[:user_id] = user.id
+    redirect_to root_path
   end
 
   def logout
+    session[:user_id] = nil
+    flash[:success] = "Successfully logged out"
+    redirect_to root_path
   end
 end
