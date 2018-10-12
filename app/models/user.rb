6c141f4da5c8de976b1cfb5ac68ebf7d3591a7bf
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-10-12 16:47:59 -0700

    Added user validation for regex, not yet tested

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_many :votes
   validates :name, presence: true, uniqueness: { case_sensitive: false },
-    length: { in: 6..20 }
+    length: { in: 6..20 }, format: { with: /(\w|-){6,20}/, message: "Only letters, underscores, and dashes allowed" }
 end
