ac4670b8528a63fd640a015e12247e4322ee561e
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-10-12 01:53:07 -0700

    Added on_delete: :cascade column to vote migration

--- db/migrate/20181008223239_add_work_and_user_references_to_votes.rb
+++ db/migrate/20181008223239_add_work_and_user_references_to_votes.rb
@@ -1,6 +1,6 @@
 class AddWorkAndUserReferencesToVotes < ActiveRecord::Migration[5.2]
   def change
-    add_reference :votes, :work, foreign_key: true
-    add_reference :votes, :user, foreign_key: true
+    add_reference :votes, :work, foreign_key: true, on_delete: :cascade
+    add_reference :votes, :user, foreign_key: true, on_delete: :cascade
   end
 end
