8c16a4fe3f342d3433c5368a1211391d4385569a
Trang <trang.frego@gmail.com>
2018-10-12 13:42:34 -0700

    create action_view in initializers and update flash error messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,18 +3,23 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:user][:username])
 
     if user.nil?
-      user= User.create(username: params[:user][:username], date_joined: DateTime.now)
+      user= User.new(username: params[:user][:username], date_joined: DateTime.now)
       if user.save
         flash[:success] = "Successfully created new user #{user.username} with ID #{user.id}"
+        redirect_to root_path
       else
-        flash[:warning] = "A problem occurred: Could not log in. Username can't be blank"
+        flash.now[:warning] = "A problem occurred: Could not log in"
+        user.errors.messages.each do |field, messages|
+          flash.now[field] = messages
+        end
+        
+        render :new
       end
     else
       flash[:success] = "Successfully logged in as existing user #{user.username}"
+      redirect_to root_path
     end
-
     session[:user_id] = user.id
-    redirect_to root_path
   end
 
   def new
