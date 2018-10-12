18202e39cabc940ce9fe2c4c7ca2272c856577ba
Hayden Williams <hwilliams.deva@gmail.com>
2018-10-12 12:14:18 -0700

    add validations to Medium

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -1,5 +1,8 @@
 class Medium < ApplicationRecord
   has_many :votes
+  validates :title, uniqueness: true, presence: true
+  validates :publication_year, numericality: true, presence: true
+  validates :category, presence: true
 
   def self.spotlight
     self.all.max_by do |medium|
