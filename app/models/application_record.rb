92067a9beb63d880da4010a5de9b5189eff4d391
Chantelle Belic <chantelle.belic@gmail.com>
2018-10-12 14:16:32 -0700

    chanegd to class method for applicationwide use

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,7 +2,7 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   #application-wide method to format dates
-  def format_date(given_date)
+  def self.format_date(given_date)
     return given_date.strftime("%B %d, %Y")
   end
 
