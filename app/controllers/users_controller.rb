8598a19c40c50e63f628ad8b4b26aedb0e824a5f
Chantelle Belic <chantelle.belic@gmail.com>
2018-10-12 12:12:53 -0700

    added controller #show action for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,22 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def show
+    @user = User.find_by(id: params[:id])
+
+    if @user == nil
+      flash.now[:failure]
+      render 'layouts/invalid_page', status: :not_found
+    end
+  end
+
+  private
+
+  #strong params
+  def user_params
+    return params.require(:work).permit(
+      :username
+    )
+  end
 end
