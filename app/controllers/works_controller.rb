528d04d3adbb5a7baa14c9a02897acf061ddbbed
Xtina <2006peacegypsy@gmail.com>
2018-10-11 22:24:09 -0700

    adding views and checking routes

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -3,8 +3,7 @@ class WorksController < ApplicationController
     @works = Work.all
   end
   def show
-    id = params[:id].to_i
-    @work = Work.find_by(id: id)
+    @work = Work.find(params[:id])
 
     if @work.nil?
       render :notfound, status: :not_found
@@ -14,7 +13,27 @@ class WorksController < ApplicationController
     @work = Work.new
   end
 
+  def create
+    @work = Wook.new(work_params)
+
+   if @work.save
+      redirect_to :action => 'list'
+   else
+      @subjects = Subject.all
+      render :action => 'new'
+   end
+  end
+
   def edit
     @work = Work.find(params[:id].to_i)
   end
+
+  def update
+  end
+
+  def delete
+  end
+  def work_params
+   params.require(:works).permit(:category, :title, :creator, :publication_year, :description)
+end
 end
