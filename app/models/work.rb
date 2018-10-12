6528958152fd1d5098a9bb39468bfd519066e9fb
Hannah <hannahvanseeters@gmail.com>
2018-10-11 18:12:43 -0700

    added links to homepage then fixed view window to add a new work to match example

--- app/models/work.rb
+++ app/models/work.rb
@@ -8,13 +8,30 @@ class Work < ApplicationRecord
   validates :description, presence: true
   validates :category, presence: true
 
-  def self.albums
-
+  def self.top_albums
+    albums = Work.where(category: "album")
+    sorted_albums = albums.sort_by { |album| album.votes.count }
+    descending_albums = sorted_albums.reverse
+    return descending_albums[0..9]
   end
 
-  def self.books
+  def self.top_books
+    books = Work.where(category: "book")
+    sorted_books = books.sort_by { |book| book.votes.count }
+    descending_books = sorted_books.reverse
+    return descending_books[0..9]
   end
 
-  def self.movies
+  def self.top_movies
+    movies = Work.where(category: "movie")
+    sorted_movies = movies.sort_by { |movie| movie.votes.count }
+    descending_movies = sorted_movies.reverse
+    return descending_movies[0..9]
+  end
+
+  def self.top_work
+    works = Work.all
+    sorted_works = works.sort_by { |work| work.votes.count }
+    return sorted_works.last
   end
 end
