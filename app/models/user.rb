dcbb10ffece734a4c1dc9d4e83b1c5d28cf5e655
BarbaraWidjono <barbarawidjono@gmail.com>
2018-10-11 21:16:56 -0700

    Display voted media for a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,32 @@ class User < ApplicationRecord
     vote = Vote.where(user_id: self.id, work_id: input_work_id)
     return vote
   end
+
+  def array_of_votes_id
+    votes = Vote.where(user_id: self.id)
+    return votes
+  end
+
+  def works_upvoted_ids
+    upvotes = array_of_votes_id
+    works_ids = []
+
+    upvotes.each do |upvote|
+      works_ids << upvote.work_id
+    end
+
+    return works_ids
+  end
+
+  def works_voted_on
+    works_ids = works_upvoted_ids
+    works = []
+
+    works_ids.each do |work_id|
+      work = Work.find_by(id: work_id)
+      works << work
+    end
+
+    return works
+  end
 end
