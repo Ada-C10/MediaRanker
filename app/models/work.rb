af24d30805ce797fef6e7b8aa7e55a649a0f86d3
Katie Jahanmir <katietj@gmail.com>
2018-10-12 06:56:27 -0700

    adjusted methods so works are sorted by vote count

--- app/models/work.rb
+++ app/models/work.rb
@@ -5,15 +5,25 @@ class Work < ApplicationRecord
   validates :publication_year, presence: true, inclusion: { in: 1000..Date.today.year, message: "should be a four-digit year"}
 
   def self.albums
-    return Work.select {|work| work.category == "album"}
+    albums =  Work.select {|work| work.category == "album"}
+    sorted_albums =  albums.sort_by{|work| work.votes.length}
+    return sorted_albums.reverse
+
   end
 
   def self.books
-    return Work.select {|work| work.category == "book"}
+    books =  Work.select {|work| work.category == "book"}
+    sorted_books =  books.sort_by{|work| work.votes.length}
+    return sorted_books.reverse
+
   end
 
   def self.movies
-    return Work.select {|work| work.category == "movie"}
+    movies =  Work.select {|work| work.category == "movie"}
+    sorted_movies =  movies.sort_by{|work| work.votes.length}
+    return sorted_movies.reverse
+
 
   end
+
 end
