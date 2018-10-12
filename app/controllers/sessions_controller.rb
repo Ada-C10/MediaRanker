2dbc9bc89d9a80c183ce0264402bdfa5ea6ad206
Laura <larmartinez2@gmail.com>
2018-10-12 15:51:00 -0700

    log in works, but log out doesn't

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,17 @@
 class SessionsController < ApplicationController
   def login
+    # find user in database to see if the user is an existing user or we need to create a new one
+    user = User.find_by(username: params[:user][:name])
+
+    if user.nil?
+      # Create a new author
+      user = User.create(username: params[:user][:name])
+      flash.now[:success] = "Successfully created a new user #{user.username} with ID #{user.id}!"
+    end
+
+    session[:user_id] = user.id
+    flash[:success] = "Successfully logged in as an existing user #{user.username}"
+    redirect_to root_path
   end
 
   def new
@@ -7,5 +19,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:success] = 'Successfully logged out'
+    redirect_back fallback_location: root_path
   end
 end
--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -3,6 +3,7 @@ class WorksController < ApplicationController
     @album = Work.select { |item| item.category == "album"}
     @movie = Work.select { |item| item.category == "movie"}
     @book = Work.select { |item| item.category == "book"}
+    @current_user = User.find_by (id:session[:user_id])
   end
 
   # we should actually make them inactive instead of deleting them
@@ -52,10 +53,24 @@ class WorksController < ApplicationController
   end
 
   def upvote
+    # @work = Work.find_by (id: params[:id])
+    # @user = User.find_by (id: sessions[:user_id])
+    #
+    # if @user
+    #   vote = Vote.new
+    #   # vote.work_id = @work.id
+    #   vote.work = @work
+    #   vote.user = @user
+    #   vote.save
+    #   @user.votes << vote
+    # else
+    #   flash[:erro] = "you are not logged in"
+    #   redirect_to session_path
+    # end
   end
 
   private
   def work_params
-    return params.require(:work).permit(:title, :category, :creator, :publication_year, :description)
+    return params.require(:work).permit(:title, :category, :creator, :publication_year, :description, :user_id)
   end
 end
