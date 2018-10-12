a7c8b700b11c8b4619dc701b092fd72c6be9e2de
Danielle Metzner <danimetzner@gmail.com>
2018-10-12 14:33:11 -0700

    Added upvote route to route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,8 @@ resources :users
   post 'users/login', to: 'users#login', as: 'login'
   delete 'users/destroy', to: 'users#destroy', as: 'logout'
 
+
+post 'works/upvote', to: 'works#upvote', as: 'upvote'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
