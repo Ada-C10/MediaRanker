ce975b5f1cccb82b165d7542af362ae57e3472b1
Hayden Williams <hwilliams.deva@gmail.com>
2018-10-12 15:20:58 -0700

    add new user route and create action to UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(params[:id])
+
+    if user
+      session[:user_id] = user.id
+    else
+      user = User.create(name: params[:id])
+      session[:user_id] = user.id
+    end
+  end
 end
