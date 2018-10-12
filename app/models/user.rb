8fc16aeed22b016248118d43e50876c7dc84fb8a
Naheed Arang <arang.na@gmail.com>
2018-10-12 14:46:14 -0700

    add validation tests for user, pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_many :votes
+  has_many :works
   has_many :liked_posts, through: :votes
+  validates :username, presence: true, uniqueness: true
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,50 @@
 require "test_helper"
 
 describe User do
-  let(:user) { User.new }
+  describe 'validations' do
+    before do
+      @user = User.new(username: 'test')
+    end
 
-  it "must be valid" do
-    value(user).must_be :valid?
+    it 'is invalid without a username' do
+      @user.username = nil
+
+      result = @user.valid?
+
+      expect(result).must_equal false
+      expect( @user.errors.messages ).must_include :username
+
+    end
+
+    it 'is invalid with a non-unique username' do
+      @user.username = User.first.username
+      result = @user.valid?
+      expect(result).must_equal false
+      expect( @user.errors.messages ).must_include :username
+    end
   end
 end
+
+
+# require "test_helper"
+#
+# describe Work do
+#   describe 'validations' do
+#     before do
+#       # Arrange
+#       @work = Work.new(title: 'test')
+#     end
+#
+#     it 'is invalid without a title' do
+#     # Arrange
+#     @work.title = nil
+#
+#     # Act
+#     result = @work.valid?
+#
+#     # Assert
+#     expect(result).must_equal false
+#     expect(@work.errors.messages).must_include :title
+#     end
+#   end
+# end
