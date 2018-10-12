bda3b4b8b0ef3861eebd3da6c51c5bce1c051473
BarbaraWidjono <barbarawidjono@gmail.com>
2018-10-11 20:50:37 -0700

    Display dates of upvotes for a specific work

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   has_many :votes
 
   validates :username, presence: true
+
+  def vote(input_work_id)
+    vote = Vote.where(user_id: self.id, work_id: input_work_id)
+    return vote
+  end
 end
