ac047649d3e0683eba851f1edeb1be11c958724a
BarbaraWidjono <barbarawidjono@gmail.com>
2018-10-12 14:21:54 -0700

    Test for class Work, find_work_id

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -2,15 +2,29 @@ require 'test_helper'
 
 
 describe Work do
+
+  describe 'relations' do
+    it 'each work can have many votes' do
+      votes = Vote.where(work_id: "sound_of_music")
+      expect(votes.count).must_equal 2
+    end
+  end
+
   describe 'validations' do
 
     before do
       @work = Work.new(title: 'casper babypants', category: 'album')
     end
 
-    it 'is valid when title and category are present' do
+    it 'is valid when title is unique and category are present' do
       is_valid = @work.valid?
       expect(is_valid).must_equal true
+    end
+
+    it 'is invalid if the title is not unique' do
+      new_work = Work.new(title: "sound of music", category: "movie")
+      is_valid = new_work.valid?
+      expect(is_valid).must_equal false
     end
 
     it 'is not valid without a title' do
@@ -24,18 +38,14 @@ describe Work do
       is_valid = @work.valid?
       expect(is_valid).must_equal false
     end
+  end
 
-    it 'is invalid if the title is not unique' do
-      new_work = Work.new(title: "sound of music", category: "movie")
-      is_valid = new_work.valid?
-      expect(is_valid).must_equal false
+  describe 'custom_methods' do
+    it 'returns the work id' do
+      work = Work.find_by(title: "sound of music")
+      work_id = work.find_work_id
+      expect(work_id).must_be_kind_of Integer
     end
   end
 
-  describe 'relations' do
-    it 'each work can have many votes' do
-      votes = Vote.where(work_id: "sound_of_music")
-      expect(votes.count).must_equal 2
-    end
-  end
 end
