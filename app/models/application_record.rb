406f4415faf5a1ac926f2d1c3620cc63cb6c889f
Chantelle Belic <chantelle.belic@gmail.com>
2018-10-12 14:09:29 -0700

    put date formatting in application wide instance method

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,9 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  #application-wide method to format dates
+  def format_date(given_date)
+    return given_date.strftime("%B %d, %Y")
+  end
+
 end
