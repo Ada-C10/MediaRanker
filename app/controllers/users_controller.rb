a58e9981a3a1cc3fd1aaf85126be8eab55caa100
Hayden Williams <hwilliams.deva@gmail.com>
2018-10-12 15:48:24 -0700

    add login functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,16 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.find_by(params[:id])
+    user = User.find_by(name: params[:name])
 
     if user
       session[:user_id] = user.id
+      redirect_to root_path
     else
-      user = User.create(name: params[:id])
+      user = User.create(name: params[:name])
+
       session[:user_id] = user.id
+      redirect_to root_path
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "pages#show", page: "home"
   resources :media
-  resources :users, only: [:index, :new]
+  resources :users, only: [:index, :new, :create]
   post '/media/:id/upvote', to: 'users#upvote', as: :upvote
   get "/pages/:page" => "pages#show"
 end
