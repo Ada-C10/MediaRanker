7d1495ebce229b9852aa6393db4340b524a167fb
BarbaraWidjono <barbarawidjono@gmail.com>
2018-10-12 13:45:24 -0700

    Added data to .yml files

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -25,5 +25,17 @@ describe Work do
       expect(is_valid).must_equal false
     end
 
+    it 'is invalid if the title is not unique' do
+      new_work = Work.new(title: "sound of music", category: "movie")
+      is_valid = new_work.valid?
+      expect(is_valid).must_equal false
+    end
+  end
+
+  describe 'relations' do
+    it 'each work can have many votes' do
+      votes = Vote.where(work_id: "sound_of_music")
+      expect(votes.count).must_equal 2
+    end
   end
 end
