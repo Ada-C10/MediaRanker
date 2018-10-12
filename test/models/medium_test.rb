53a7722c12ed808e3ea39d11b705635ed584ac1d
Hayden Williams <hwilliams.deva@gmail.com>
2018-10-12 14:04:51 -0700

    add tests for relations and custom methods for Medium

--- test/models/medium_test.rb
+++ test/models/medium_test.rb
@@ -3,6 +3,7 @@ require "test_helper"
 describe Medium do
 
   describe 'validations' do
+
     before do
       @medium = Medium.new(
         title: 'CyberBabies on High',
@@ -73,6 +74,53 @@ describe Medium do
       expect( is_valid ).must_equal false
       expect( @medium.errors.messages ).must_include :publication_year
     end
+
+  end
+
+  describe 'relations' do
+
+    before do
+      @medium = media(:one)
+      medium_id = @medium.id
+
+      # create 3 vote instances for @medium
+      Vote.create!(user_id: users(:one).id, medium_id: medium_id)
+      Vote.create!(user_id: users(:two).id, medium_id: medium_id)
+      Vote.create!(user_id: users(:three).id, medium_id: medium_id)
+    end
+
+    it 'can access votes through instance method' do
+      votes = @medium.votes
+      expect( votes.length ).must_equal 3
+    end
+
+  end
+
+  describe 'spotlight self method' do
+
+    before do
+      @medium = media(:one)
+      medium_id = @medium.id
+
+      @medium2 = media(:two)
+      medium2_id = @medium2.id
+
+      # create 2 vote instances for @medium
+      Vote.create!(user_id: users(:one).id, medium_id: medium_id)
+      Vote.create!(user_id: users(:two).id, medium_id: medium_id)
+
+      # create 3 vote instances for @medium2
+      Vote.create!(user_id: users(:two).id, medium_id: medium2_id)
+      Vote.create!(user_id: users(:three).id, medium_id: medium2_id)
+      Vote.create!(user_id: users(:one).id, medium_id: medium2_id)
+    end
+
+    it 'returns the Medium instance with the most votes' do
+      spotlight = Medium.spotlight
+
+      expect( spotlight ).must_equal @medium2
+    end
+
   end
 
 end
