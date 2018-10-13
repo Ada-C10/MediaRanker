6e77e3466a33400d8ddf114c8b6c9e9c6d75f406
Laura <larmartinez2@gmail.com>
2018-10-13 14:21:23 -0700

    create route for upvote

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   root 'pages#index'
   post 'sessions/login', to: 'sessions#login', as: 'login'
   get 'sessions/login', to: 'sessions#new'
+  get 'works/:id/upvote', to: 'works#upvote', as: 'upvote'
   delete 'sessions/destroy', to: 'sessions#destroy', as: 'logout'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :works
