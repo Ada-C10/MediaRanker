479901ab2ad92aec3ef9bd6dfee2825e5fe25c30
BarbaraWidjono <barbarawidjono@gmail.com>
2018-10-12 20:40:59 -0700

    Tests for class User relations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,8 +1,21 @@
 require "test_helper"
 
 describe User do
-  describe 'validations' do
 
+  describe 'relations' do
+    it 'a user can have many votes' do
+      user1 = users(:june)
+      work1 = works(:sound_of_music)
+      work2 = works(:green_eggs)
+      vote1 = Vote.create!(user_id: user1.id, work_id: work1.id)
+      vote2 = Vote.create!(user_id: user1.id, work_id: work2.id)
+      votes = Vote.where(user_id: user1.id)
+
+      expect(votes.count).must_equal 2
+    end
+  end
+
+  describe 'validations' do
     # note to self: .valid? checks against yaml file
     it 'is a valid user if the username is present and unique' do
       new_user = User.new(username: "toby")
@@ -21,5 +34,7 @@ describe User do
       is_valid = new_user.valid?
       expect(is_valid).must_equal false
     end
+
   end
+
 end
