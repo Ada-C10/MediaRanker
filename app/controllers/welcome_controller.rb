d0391170aaba2342f3e5040848bbbc340fd50be3
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-10-13 10:38:04 -0700

    Completed spotlight and welcome page formatting

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,5 +6,17 @@ class WelcomeController < ApplicationController
     @movies = @works.select { |work| work.category == "movie" }
     @albums = @works.select { |work| work.category == "album" }
     @books = @works.select { |work| work.category == "book" }
+    @spotlight = top_work
   end
+
+  def top_work
+    # Access array of works
+    # Select work with most votes
+    # If no votes for any works, select random work
+    if @works.all? {|work| work.votes.empty? }
+      return @works[ rand(0..( works.length-1 )) ]
+    else
+      @works.max_by { |work| work.votes.length }
+    end
+end
 end
