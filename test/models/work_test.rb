7598cc68c1b39f608afaa983e6d664897dbb6e06
Valerie Gidzinski <valgidzi@gmail.com>
2018-10-13 15:26:12 -0700

    Additional validation assertions

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -9,26 +9,37 @@ describe Work do
 
     it 'is valid when all required field are present' do
       expect(@work.valid?).must_equal true
+      expect(@work.errors.messages).must_be_empty
     end
 
     it 'is invalid without category' do
       @work.category = nil
       expect(@work.valid?).must_equal false
+      expect(@work.errors.messages).must_include :category
     end
 
     it 'is invalid without title' do
       @work.title = nil
       expect(@work.valid?).must_equal false
+      expect(@work.errors.messages).must_include :title
     end
 
     it 'is invalid without creator' do
       @work.creator = nil
       expect(@work.valid?).must_equal false
+      expect(@work.errors.messages).must_include :creator
     end
 
     it 'is invalid without integer publication year' do
       @work.publication_year = "ninety"
       expect(@work.valid?).must_equal false
+      expect(@work.errors.messages).must_include :publication_year
     end
   end
+
+  # desribe 'relations' do
+  #   before do
+  #   end
+  #   it 'responds to '
+  # end
 end
