35d6ddc985727d7ccf31ad7a226be17b58ca9d2b
Katricia Smith <smithkatricia@gmail.com>
2018-10-13 15:38:15 -0700

    Add Sessions#login and Sessions#logout actions to Sessions Controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,17 @@
 class SessionsController < ApplicationController
+
+  def login
+    user = User.find_by(username: params[:username])
+
+    if user
+      session[:user_id] = user.id
+      flash[:sucess] = "Successfully created new user #{user.username} with ID #{user.id}"
+      redirect_to root_path
+    end
+  end
+
+  def logout
+    session[:user_id] = nil
+  end
+
 end
