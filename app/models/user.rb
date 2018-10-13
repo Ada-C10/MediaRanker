363126e1a7c090d266e8101105a61a7fa60dd578
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-10-12 21:08:06 -0700

    Index page shows the rows we want

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ApplicationRecord
   has_many :votes
   validates :name, presence: true, uniqueness: { case_sensitive: false },
     length: { in: 6..20 }, format: { with: /(\w|-){6,20}/, message: "Only letters, numbers, underscores, and dashes allowed" }
+
+  def print_number_of_votes
+    if self.votes.size != 1
+      return "#{self.votes.size} votes"
+    else
+      return "#{self.votes.size} vote"
+    end
+  end
+
 end
