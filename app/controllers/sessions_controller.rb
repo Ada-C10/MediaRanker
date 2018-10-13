76b995d111443153d3c9f39d07b2a607be0917c7
Carly Reiter <yepodacarly@gmail.com>
2018-10-13 15:31:02 -0700

    User can log in and button will say their name.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -29,7 +29,7 @@ class SessionsController < ApplicationController
 
   def logout
     session[:user_id] = nil
-    flash[:success] = "Successfully logged out"
+    flash[:success] = "Successfully logged out!"
     redirect_to root_path
   end
 
