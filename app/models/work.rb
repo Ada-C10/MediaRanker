6c0468b461d3651e6151a44903712939fa4e68d6
Jessie Zhang <zhang_jessie2017@outlook.com>
2018-10-13 14:29:53 -0700

    completed the spotlight item selection method

--- app/models/work.rb
+++ app/models/work.rb
@@ -19,4 +19,25 @@ class Work < ApplicationRecord
   def self.movie_list
      return Work.all.select {|work| work.category == "movie"}.sort_by {|e| e.votes.count}.reverse
   end
+
+  def self.top_one
+    top = []
+    top_album = self.album_list.first
+    top_book = self.book_list.first
+    top_movie = self.movie_list.first
+    if !top_album.nil?
+      top << top_album
+    end
+
+    if !top_book.nil?
+      top << top_book
+    end
+
+    if !top_movie.nil?
+      top << top_movie
+    end
+
+    spotlight = top.max_by {|e| e.votes.count}
+    return spotlight
+  end
 end
