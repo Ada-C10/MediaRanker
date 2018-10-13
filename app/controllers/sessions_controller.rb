cef25fec84a541fa1cf09cd19a98a734f9d10999
Hannah <hannahvanseeters@gmail.com>
2018-10-12 18:12:45 -0700

    edited login to work

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,12 +14,11 @@ class SessionsController < ApplicationController
         end
         redirect_to login_path
       end
-    else
+
       flash[:success] = "Successfully logged in as existing user #{user.user_name}"
       session[:user_id] = user.id
       redirect_to root_path
     end
-
 
   end
 
