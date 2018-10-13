83503a5dd1ad4f89158328362d3bc22f97eac5f6
Valerie Gidzinski <valgidzi@gmail.com>
2018-10-13 15:38:05 -0700

    Add user model validation tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,11 +1,11 @@
 require "test_helper"
 
 describe User do
+  before do
+    @user = users(:val)
+  end
 
   describe 'validations' do
-    before do
-      @user = users(:val)
-    end
 
     it 'is valid when all required field are present' do
       expect(@user.valid?).must_equal true
@@ -16,6 +16,18 @@ describe User do
       @user.name = nil
       expect(@user.valid?).must_equal false
       expect(@user.errors.messages).must_include :name
+    end
+
+  end
+
+  describe 'relations' do
+
+    it 'responds to votes method' do
+      expect(@user.votes).must_respond_to :each
+    end
+
+    it 'responds to upvoted_works method' do
+      expect(@user.upvoted_works).must_respond_to :each
     end
 
   end
