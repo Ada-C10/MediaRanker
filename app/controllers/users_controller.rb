e8363583e50fbf34fd91e58d85bb5e458bd1c9ad
Katricia Smith <smithkatricia@gmail.com>
2018-10-13 14:48:50 -0700

    Add Users controller index, new, create, and show actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,31 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+
+  def new
+    @user = User.new
+  end
+
+
+  def create
+    @user = User.new
+
+    if @user.save
+      flash[:success] = "Successfully created new user #{@user.username} with ID #{@user.id}"
+      redirect_to works_path
+    else
+      flash.now[:error] = "A problem occurred: Could not create #{@user.username}"
+      render :new, status: :bad_request
+    end
+  end
+
+
+  def show
+    if @user.nil?
+      head :not_found
+    end
+  end
+
 end
