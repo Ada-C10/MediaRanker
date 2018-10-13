dc7bd30b3bdc333a5b13ab7e774485e23602bf2a
Pushpa Agr <pushpaagr108@gmail.com>
2018-10-12 19:16:07 -0700

    added logic for wave 1 to build the spotlight to display the most popular work

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -21,20 +21,17 @@ describe Vote do
     # vote = Vote.new(work_id: work.id, user_id: @logged_in_user.id)
 
     repeat_vote = Vote.new(user_id: @vote1.user.id, work_id: @vote1.work.id)
-    # binding.pry
+
     is_valid = repeat_vote.valid?
     expect( is_valid ).must_equal false
   end
 
 
-  it "allows 2 different users to vote for the same work" do
-
-    # vote = Vote.new(work_id: work.id, user_id: @logged_in_user.id)
+  it "It allows 2 different users to vote for the same work" do
     @vote3 = votes(:vote3)
 
-
     same_work_vote = Vote.new(user_id: @vote1.user.id, work_id: @vote3.work.id)
-    # binding.pry
+
     is_valid = same_work_vote.valid?
     expect( is_valid ).must_equal true
   end
