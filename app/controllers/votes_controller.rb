fc9fdd666d1c8b7627875ed10222ec701a07c1c4
Hannah <hannahvanseeters@gmail.com>
2018-10-12 20:25:09 -0700

    adjusted routes to only include routes needed and changed date view for user summary

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,7 +1,4 @@
 class VotesController < ApplicationController
   def index
   end
-  # need to add upvote as a column
-
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,10 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#login', as: 'login'
   get '/login', to: 'sessions#new', as: 'new_session'
   delete 'sessions/destroy', to: 'sessions#destroy', as: 'logout'
-  # need to update below to exclude routes we do not need
+
+  resources :users, only: [:index, :show]
   resources :works
-  resources :users
-  resources :votes
-
   post 'works/:id/upvote', to: 'works#upvote', as: 'upvote'
-  root to: 'pages#home'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
 end
