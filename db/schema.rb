b957f1e4d1575242261b03bab114cdf80178a264
BarbaraWidjono <barbarawidjono@gmail.com>
2018-10-13 10:34:37 -0700

    Added migration to works table: total_votes column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_08_231725) do
+ActiveRecord::Schema.define(version: 2018_10_13_173110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2018_10_08_231725) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "total_votes", default: 0
   end
 
   add_foreign_key "votes", "users"
