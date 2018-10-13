be2176b057f0ea47e4ab4c34f220e8cad8d23c1f
Pushpa Agr <pushpaagr108@gmail.com>
2018-10-12 18:36:02 -0700

    added test for the Vote that is testing the realtionship that a Vote has a work_id and vote has a user_id, test passed

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -40,19 +40,17 @@ describe Vote do
   end
 
 
-  #
-  # it "allows 2 different users to vote for the same work" do
-  #
-  #   # vote = Vote.new(work_id: work.id, user_id: @logged_in_user.id)
-  #   @vote3 = votes(:vote3)
-  #
-  #
-  #   same_work_vote = Vote.new(user_id: @vote1.user.id, work_id: @vote3.work.id)
-  #   # binding.pry
-  #   is_valid = same_work_vote.valid?
-  #   expect( is_valid ).must_equal true
-  # end
+  #testing the realtionship that Vote has a user which has a user_id.
+  it "A vote has a user_id" do
+    @vote1.user_id == @vote1.user.id
+    expect(@vote1.user_id == @vote1.user.id
+    ).must_equal true
+  end
 
+  #testing the realtionship that a Vote has a Work which has work_id.
+  it "A vote has a work_id" do
+    expect(@vote1.work_id == @vote1.work.id).must_equal true
+  end
 
 
 
