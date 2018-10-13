1ff3e247a0f873b5a7e9af32c45eb36df4c6fc72
Hannah <hannahvanseeters@gmail.com>
2018-10-12 18:02:30 -0700

    edit login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,11 +12,10 @@ class SessionsController < ApplicationController
         user.errors.messages.each do |field, messages|
           flash.now[field] = messages
         end
-
         redirect_to login_path
       end
     else
-      flash["success"] = "Successfully logged in as existing user #{user.user_name}"
+      flash[:success] = "Successfully logged in as existing user #{user.user_name}"
       session[:user_id] = user.id
       redirect_to root_path
     end
