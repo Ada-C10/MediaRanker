d544fb90de6f0faa46ced0416c59da7681542dde
Katricia Smith <smithkatricia@gmail.com>
2018-10-13 16:14:13 -0700

    Add logic to Sessions methods new and login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,28 @@
 class SessionsController < ApplicationController
 
+  def new
+  end
+
   def login
-    user = User.find_by(username: params[:username])
+    name = params[:username]
+    user = User.find_by(username: name)
 
     if user
       session[:user_id] = user.id
-      flash[:sucess] = "Successfully created new user #{user.username} with ID #{user.id}"
+      flash[:sucess] = "Successfully logged in as existing user #{user.username}"
       redirect_to root_path
+    else
+        render :new
+        
+        if user.save
+        flash[:sucess] = "Successfully created new user #{user.username} with ID #{user.id}"
+        redirect_to root_path
     end
   end
 
   def logout
     session[:user_id] = nil
+    flash[:success] = "Successfully logged out"
   end
 
 end
