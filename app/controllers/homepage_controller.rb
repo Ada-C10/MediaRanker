b28b98c6bca92d6c9983828048bdc221887b25c6
BarbaraWidjono <barbarawidjono@gmail.com>
2018-10-13 11:25:08 -0700

    Ordered querying results in descending order for Works#index

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,12 +1,11 @@
 class HomepageController < ApplicationController
   def index
-    # @movies = Work.where(category: "movie")
+    
     @movies = Work.where(category: "movie").order(total_votes: :desc).take(10)
-    # @books = Work.where(category: "book")
+
     @books = Work.where(category: "book").order(total_votes: :desc).take(10)
 
     @albums = Work.where(category: "album").order(total_votes: :desc).take(10)
-    # @albums = Work.where(category: "album")
 
     @media_spotlight = Work.order(total_votes: :desc).first
   end
--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,6 +1,6 @@
 class WorksController < ApplicationController
   def index
-    @works = Work.all
+    @works = Work.all.order(total_votes: :desc)
   end
 
   def new
