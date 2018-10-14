623113519592b18515f0f6bf936d65b51f345a64
Pushpa Agr <pushpaagr108@gmail.com>
2018-10-13 18:27:03 -0700

    added first test that check for a valid user, test passed

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,17 @@
 require "test_helper"
 
 describe User do
-  let(:user) { User.new }
+  # let(:user) { User.new }
+
+  before do
+    @user1 = users(:house)
+    @user2 = users(:jones)
+    # binding.pry
+  end
 
   it "must be valid" do
-    value(user).must_be :valid?
+    value(@user1).must_be :valid?
   end
+
+
 end
