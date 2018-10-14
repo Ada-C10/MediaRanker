66306c77d242671b2f87ecea07e000ec44db36b5
Chantelle Belic <chantelle.belic@gmail.com>
2018-10-14 16:07:47 -0700

    populated methods for finding spotlight and top ten works by vote in welcomcontroller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,5 @@
 class WelcomeController < ApplicationController
 
-  before_action :find_all_works, only: :index
   before_action :find_top_ten_movies, only: :index
   before_action :find_top_ten_books, only: :index
   before_action :find_top_ten_albums, only: :index
@@ -11,21 +10,19 @@ class WelcomeController < ApplicationController
 
   private
 
-  def find_votes
-    @works ||= Work.all
-  end
-
   def find_spotlight_work
+  @spotlight_work = Work.left_joins(:votes).group(:id).order('COUNT(votes.id) DESC').limit(1)
   end
 
   def find_top_ten_movies
+      @top_ten_movies = Work.where(category: 'movie').left_joins(:votes).group(:title).order('COUNT(votes.id) DESC').limit(10)
   end
 
   def find_top_ten_books
+    @top_ten_books = Work.where(category: 'book').left_joins(:votes).group(:id).order('COUNT(votes.id) DESC').limit(10)
   end
 
   def find_top_ten_albums
+      @top_ten_albums = Work.where(category: 'album').left_joins(:votes).group(:id).order('COUNT(votes.id) DESC').limit(10)
   end
-
-
 end
