dea5f1d24d1702fdd1925a27c11cd71c90cc646c
Valerie Gidzinski <valgidzi@gmail.com>
2018-10-13 18:27:44 -0700

    Works are displayed by number of votes in descending order on main and work index pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
   def index
     @works = Work.all
-    @books = Work.where(category: 'book').first(10)
-    @albums = Work.where(category: 'album').first(10)
-    @movies = Work.where(category: 'movie').first(10)
+    @books = Work.where(category: 'book')
+    @albums = Work.where(category: 'album')
+    @movies = Work.where(category: 'movie')
   end
 end
