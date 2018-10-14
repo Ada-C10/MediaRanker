77e6605fcf1578ca6ef98a17975bdb07066a6f60
Katricia Smith <smithkatricia@gmail.com>
2018-10-13 18:42:33 -0700

    Add Votes#create

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,2 +1,24 @@
 class VotesController < ApplicationController
+
+  def create
+    @vote = Vote.new(work_id: work.id, user_id: user.id)
+
+    if @vote.save
+      flash[:success] = "Successfully upvoted!"
+      redirect_to work_path
+    else
+      flash.now[:error] = "A problem occurred: You must log in to do that"
+      render :new, status: :bad_request
+    end
+  end
+
+
+# private
+#
+#   def vote_params
+#     return params.require(:vote).permit(
+#       :work_id,
+#       :user_id,
+#     )
+#   end
 end
