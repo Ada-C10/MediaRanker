e050a97056f066d18228804228614cba685a305e
Karis Kim <ouniiq@hotmail.com>
2018-10-14 11:30:02 -0700

    created fixtures for testing validations in vote_test.rb

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,9 +1,16 @@
 require "test_helper"
 
 describe Vote do
-  let(:vote) { Vote.new }
+  describe 'relations' do
+    it "has a work" do
+      vote = votes(:one)
+      vote.work.must_equal works(:jane)
+    end
 
-  it "must be valid" do
-    value(vote).must_be :valid?
+    it "can set the work" do
+      vote = Vote.new
+      vote.work = works(:harry)
+      vote.work_id.must_equal works(:harry).id
+    end
   end
 end
