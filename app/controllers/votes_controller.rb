6128b8adaeec2648c86055f0f06b21e685cfb617
Chantelle Belic <chantelle.belic@gmail.com>
2018-10-14 14:11:01 -0700

    changed votes controller to redirect back to the page user came from

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,17 +4,17 @@ class VotesController < ApplicationController
 
     user = User.find_by(id: session[:user_id])
     work = Work.find_by(id: params[:work_id])
-
+    
     @vote = Vote.new(user: user, work: work)
     if @vote.save
       flash[:success] = "Successfully upvoted!"
-      redirect_to root_path
+      redirect_back fallback_location: root_path
     elsif !user
       flash[:failure] = "A problem occurred: You must log in to do that."
-      redirect_to root_path, status: 400
+      redirect_back fallback_location: root_path, status: 400
     else
       flash[:failure] = "A problem occurred: Could not upvote."
-      redirect_to root_path, status: 400
+      redirect_back fallback_location: root_path, status: 400
     end
   end
 end
