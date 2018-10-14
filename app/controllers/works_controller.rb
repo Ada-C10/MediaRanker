fb5b71cfc6756d796b5b74c9a71fd12df0be3e1c
Carly Reiter <yepodacarly@gmail.com>
2018-10-14 09:50:28 -0700

    finally able to create a vote.

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -64,11 +64,14 @@ class WorksController < ApplicationController
 
   def upvote
     # if @user.id == session[:user_id]
-
+    @work = Work.find_by(params[:id])
     # user = User.find_by(id: session[:user_id])
-    # work = Work.find_by(id: params[:id])
+    # @work = Work.find_by(id: params[:id])
     # @vote = Vote.new(work_id: work.id, user_id: user.id)
-    @vote = Vote.new(user_id: @logged_in_user.id, work_id: @work.id)
+    # @vote = Vote.new(user_id: @logged_in_user.id, work_id: @work.id)
+    @vote = Vote.new
+    @vote.user_id = @logged_in_user.id
+    @vote.work_id = @work.id
 
     if @vote.save
       flash[:success] = "Successfully voted!"
