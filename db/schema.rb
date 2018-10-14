a7a2ceeb0ef8232d80aefd98f9a20b1e0a90ea53
Xtina <2006peacegypsy@gmail.com>
2018-10-13 22:04:35 -0700

    updated column name in votes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_13_201753) do
+ActiveRecord::Schema.define(version: 2018_10_14_045705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2018_10_13_201753) do
   end
 
   create_table "votes", force: :cascade do |t|
-    t.integer "topic_id"
+    t.integer "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
