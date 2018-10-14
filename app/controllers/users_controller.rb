f59cd8dee4e103077ce7c1688754796ef0424f4f
Anibel America <ameriabel@gmail.com>
2018-10-14 15:45:41 -0700

    removes unnecessary routes and actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,21 +8,4 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
   end
 
-  def create
-    user = User.new(user_params)
-
-    user.save
-
-    redirect_back(fallback_location: new_session_path)
-
-  end
-
-  private
-
-  def user_params
-    return params.require(:user).permit(
-      :username
-    )
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "homepage#index", as: 'home'
 
-  resources :users, :works, :votes
+  resources :works, :votes
+
+  resources :users, only: [:index, :show]
 
   post '/works/:work_id/votes', to: 'votes#create', as: 'create_work_vote'
 
