1a5878acb8b2d53e9793098b433d745e1cb48ab4
Kay <kayxn23@gmail.com>
2018-10-13 18:31:39 -0700

    wrapped SQL strings in Arel.sql calls so that its not a dangerous query method

--- app/models/work.rb
+++ app/models/work.rb
@@ -5,51 +5,16 @@ class Work < ApplicationRecord
   has_many :users, through: :votes
   has_many :votes
 
-  #this one is working to sort by title 
-  default_scope {order(title: :asc)}
-
-
-  # , -> {group 'works.id'} default_scope {order(title: :desc)}
-  # default_scope where(color: 'red')`, please use `default_scope { where(color: 'red') }
-  # has_many :votes, -> {group 'works.id'}.order('count(works.id) DESC', 'title ASC')
-
-
   def self.albums
-    # Company.joins(:jobs).group("companies.id").order("count(companies.id) DESC")
-    return Work.where(category: 'album') # Work is the table
-    # return Work.joins(:votes).group("works.id").order("count(works.id) DESC")
-    # return Work.joins(:votes).group('works.id').order('count(works.id) DESC', 'title ASC')
-    #i want to return a list of albums that are sorted by vote count and title
+    return Work.where(category: 'album').order(Arel.sql('votes_count IS NULL, votes_count DESC'), title: :asc)
   end
 
   def self.books
-    return Work.where(category: 'book')
+    return Work.where(category: 'book').order(Arel.sql('votes_count IS NULL, votes_count DESC'), title: :asc)
   end
 
   def self.movies
-    return Work.where(category: 'movie')
+    return Work.where(category: 'movie').order(Arel.sql('votes_count IS NULL, votes_count DESC'), title: :asc)
   end
-
-  def sort_by_vote_count
-    #sort any list of work objects by title and votes
-    #first by votes
-    #second priority by title
-    return Work.order(votes: :desc, title: :asc)
-    #how can i call this method on the albums and books lists?
-  end
-
 
 end
-
-# def sort_by_vote_count
-#   #this method sorts vote count in descending order & alphabetically
-#   #it needs to be in alphabetical order by vote count (by multiple conditions)
-#   # sort_by { |obj| block } → array
-#   # users.sort_by { |user| [user.age, user.name] }
-#   # ary.sort_by {|name, age| [age, name] }
-#   #list = self.sort #sorts alphabetically from a-z
-#   #then sort by vote number
-#   #check how many votes each work has
-#   #sort from greatest to smallest
-#   return list.sort_by {|work| [work.votes.count, work.title]}
-# end
