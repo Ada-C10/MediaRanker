a7feaed13fd2721c18144ee2c160483dacaac31e
Sigrid Benezra <sdbenezra@gmail.com>
2018-10-14 00:34:01 -0700

    Created methods in work to sort works by vote totals.

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,3 @@
-require 'pry'
 class Work < ApplicationRecord
   # relation - user can have many votes
   has_many :votes
@@ -6,12 +5,18 @@ class Work < ApplicationRecord
   validates :title, presence: true, uniqueness: true
 
   def self.list_works_by_category(category)
-    works_by_category = []
-    work = Work.all
-    work.each do |work|
-      works_by_category <<
-     work unless work[:category] != category
-    end
+    works = Work.all
+    works_by_category = works.select { |work| work.category == category }
     return works_by_category
+  end
+
+  def self.order_works_by_category(category)
+    works_by_category = Work.list_works_by_category(category)
+    sorted_works_by_category = works_by_category.sort_by { |work| work.votes.size }.reverse
+    return sorted_works_by_category
+  end
+
+  def self.top_works_by_category(number)
+    return Work.order_works_by_category.take(number)
   end
 end
