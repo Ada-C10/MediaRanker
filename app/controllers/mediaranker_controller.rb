41e896bdde06058cd64dde4cea78a87b6a50329b
Dionisia Montanez <dmontanez15@gmail.com>
2018-10-14 12:21:13 -0700

    added top ten to main page with styling

--- app/controllers/mediaranker_controller.rb
+++ app/controllers/mediaranker_controller.rb
@@ -1,5 +1,8 @@
 class MediarankerController < ApplicationController
-  def index
-
+  def main
+    @works = Work.all
+    @movies = (Work.where(category: "movie")).ordering.limit(10)
+    @books = (Work.where(category: "book")).ordering.limit(10)
+    @albums = (Work.where(category: "album")).ordering.limit(10)
   end
 end
