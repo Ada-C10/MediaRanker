e94fd517c62ebec3d8dd002697cee4b5e2bc523c
Lindsay Terchin <lterchin@gmail.com>
2018-10-14 16:24:20 -0700

    wrote relations tests for vote

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,9 +1,30 @@
 require "test_helper"
 
 describe Vote do
-  let(:vote) { Vote.new }
 
-  it "must be valid" do
-    value(vote).must_be :valid?
+  describe 'relations' do
+
+    it "belongs to a work" do
+
+      work = Work.first
+      votes = work.votes
+
+      expect(votes).must_respond_to :each
+
+    end
+
+    it "belongs to a user" do
+
+      user = User.first
+      votes = user.votes
+
+      expect(votes).must_respond_to :each
+
+    end
+
   end
+
+
+
+
 end
