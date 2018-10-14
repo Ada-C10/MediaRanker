6af3481ebb064ca8a319855fc2588e165b3167f1
Pushpa Agr <pushpaagr108@gmail.com>
2018-10-13 18:45:33 -0700

    added test that is valid when user has many votes

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,4 +1,5 @@
 require "test_helper"
+require 'pry'
 
 describe User do
   # let(:user) { User.new }
@@ -9,9 +10,38 @@ describe User do
     # binding.pry
   end
 
+  #
+  # has_many :votes
+  #
+  # validates :username, presence: true, uniqueness: true, allow_nil: true
+  #
+
   it "must be valid" do
     value(@user1).must_be :valid?
   end
+
+
+  it "It is valid when user has many  votes" do
+
+    @work1 = Work.new(title: "The name of the wind")
+
+
+    @work1 = Work.new(title: "Ruby on Rails")
+
+    user_vote1 = Vote.new(user_id: @user1.id, work_id: @work1.id)
+
+    user_vote2 = Vote.new(user_id: @user1.id, work_id: @work1.id)
+
+    # binding.pry
+    value(@user1).must_be :valid?
+    value(@user2).must_be :valid?
+
+
+
+
+  end
+
+
 
 
 end
