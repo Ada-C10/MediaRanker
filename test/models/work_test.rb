303f914a210ac243990a6f78afc542ab9f2fc429
Karis Kim <ouniiq@hotmail.com>
2018-10-13 17:41:45 -0700

    created test to check uniqueness for new work title. passed test

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -1,34 +1,46 @@
 require "test_helper"
 
 describe Work do
-  before do
+  describe 'validations' do
+    before do
       # Arrange
       @work = Work.new(category: 'book', title: 'nice title')
-  end
+    end
 
-  it 'is valid when all fields are present' do
+    it 'is valid when all fields are present' do
       # Act
       result = @work.valid?
 
       # Assert
       expect(result).must_equal true
-  end
+    end
 
-  it 'is invalid without a category' do
-  # Arrange
-  @work.category = nil
+    it 'is invalid without a category' do
+      # Arrange
+      @work.category = nil
 
-  # Act
-  result = @work.valid?
+      # Act
+      result = @work.valid?
 
-  # Assert
-  expect(result).must_equal false
-  end
+      # Assert
+      expect(result).must_equal false
+      expect(@work.errors.messages).must_include :category
+    end
 
-  it "is invalid without a title" do
-    @work.title = nil
-    result = @work.valid?
+    it "is invalid without a title" do
+      @work.title = nil
+      result = @work.valid?
 
-    expect(result).must_equal false
+      expect(result).must_equal false
+      expect(@work.errors.messages).must_include :title
+    end
+
+    it "is invalid if same title already exists" do
+      @work.save
+      @new_work = Work.new(category: 'book', title: @work.title)
+      result = @new_work.valid?
+
+      expect(result).must_equal false
+    end
   end
 end
