475ce09ad9922776a8cf62bb2221ca136bd70edd
Karis Kim <ouniiq@hotmail.com>
2018-10-14 11:33:37 -0700

    added fixture for user n added userid for votes yml, created n passed tests for user-vote relation

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -12,5 +12,17 @@ describe Vote do
       vote.work = works(:harry)
       vote.work_id.must_equal works(:harry).id
     end
+
+    it "has a user" do
+      vote = votes(:one)
+      vote.user.must_equal users(:karis)
+    end
+
+    it "can set the user" do
+      vote = Vote.new
+      vote.user = users(:harry)
+
+      vote.user_id.must_equal users(:harry).id
+    end
   end
 end
