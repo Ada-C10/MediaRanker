e7b84994470af5cea71bbf8bc3e450fefe6e65dd
Karis Kim <ouniiq@hotmail.com>
2018-10-14 12:11:20 -0700

    created n passed test for checking relation btwn user n vote model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,30 @@
 require "test_helper"
 
 describe User do
-  let(:user) { User.new }
+  describe 'relations' do
+    it 'can set the user id through new vote' do
+      # Create two models
+      @user = User.create!(username: 'puppy')
+      @vote = Vote.new
 
-  it "must be valid" do
-    value(user).must_be :valid?
+      # Make the models relate to one another
+      @vote.user_id = @user.id
+
+      # author_id should have changed accordingly
+      expect(@vote.user_id).must_equal @user.id
+    end
+
+    it "has a vote" do
+      user = users(:karis)
+      user.votes.must_include votes(:one)
+    end
+
+    it "can set the work id in vote through work" do
+      user = users(:harry)
+      vote = votes(:two)
+      user.votes << vote
+
+      user.votes.first.id.must_equal votes(:two).id
+    end
   end
 end
