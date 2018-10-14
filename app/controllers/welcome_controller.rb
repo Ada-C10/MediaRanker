97202235f8d487d54bc5c4ae5444246d34dfded1
Chantelle Belic <chantelle.belic@gmail.com>
2018-10-14 15:17:48 -0700

    added skeleton for populating methods to find top ten in welcome controller and column to vote :counter_cache => true

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,31 @@
 class WelcomeController < ApplicationController
+
+  before_action :find_all_works, only: :index
+  before_action :find_top_ten_movies, only: :index
+  before_action :find_top_ten_books, only: :index
+  before_action :find_top_ten_albums, only: :index
+  before_action :find_spotlight_work, only: :index
+
   def index
-  end 
+  end
+
+  private
+
+  def find_votes
+    @works ||= Work.all
+  end
+
+  def find_spotlight_work
+  end
+
+  def find_top_ten_movies
+  end
+
+  def find_top_ten_books
+  end
+
+  def find_top_ten_albums
+  end
+
+
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,6 +1,6 @@
 class Vote < ApplicationRecord
   belongs_to :user
-  belongs_to :work
+  belongs_to :work, :counter_cache => true
   validates :user_id, presence: true
   validates :work_id, presence: true
   validates :work, uniqueness: {
