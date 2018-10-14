ef9d099c494ddaf3474e7a7cac3bba0822c97d74
Katricia Smith <smithkatricia@gmail.com>
2018-10-13 18:45:04 -0700

    Add upvote method to Vote.rb

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,10 @@
 class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :work
+
+  def upvote
+    vote = Vote.new(work_id: @work.id, user_id: @logged_in_user.id)
+    # .votes has to be called on an instance, not on the class
+  end
+
 end
