d10f204dd3da3a8a4874e5e756797f907a67b591
Katricia Smith <smithkatricia@gmail.com>
2018-10-13 18:41:54 -0700

    Add logic for if user does not exist in Sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,12 +12,16 @@ class SessionsController < ApplicationController
       flash[:sucess] = "Successfully logged in as existing user #{user.username}"
       redirect_to root_path
     else
-        render :new
+      new_user = User.new(name: name)
+      session[:user_id] = new_user.id
+      if new_user.save
+        flash[:sucess] = "Successfully created new user #{new_user.username} with ID #{new_user.id}"
+        redirect_to root_path
 
         # if user.save
         #   flash[:sucess] = "Successfully created new user #{user.username} with ID #{user.id}"
         #   redirect_to root_path
-        # end
+        end
     end
   end
 
