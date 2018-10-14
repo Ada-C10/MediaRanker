07edc359b3fa0bf4b57919f47c2c877407ff2852
Valerie Gidzinski <valgidzi@gmail.com>
2018-10-14 04:01:30 -0700

    More Heroku debugging with work model and page views

--- app/models/work.rb
+++ app/models/work.rb
@@ -7,11 +7,4 @@ class Work < ApplicationRecord
   has_many :votes
   has_many :upvoted_users, through: :votes, source: :user
 
-  def sort_by_votes
-    return self.all.sort_by { |work| work.votes.count }.reverse
-  end
-
-  def self.spotlight
-    return Work.sort_by_votes.first
-  end
 end
