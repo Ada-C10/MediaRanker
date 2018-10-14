401010a3cfb84b1b961f0de141a96b8047a70390
Pushpa Agr <pushpaagr108@gmail.com>
2018-10-14 10:24:44 -0700

    added more tests for users

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -25,21 +25,43 @@ describe User do
 
     @work1 = Work.new(title: "The name of the wind")
 
-
     @work1 = Work.new(title: "Ruby on Rails")
 
     user_vote1 = Vote.new(user_id: @user1.id, work_id: @work1.id)
 
     user_vote2 = Vote.new(user_id: @user1.id, work_id: @work1.id)
 
-    # binding.pry
     value(@user1).must_be :valid?
     value(@user2).must_be :valid?
 
-
-
-
   end
+
+
+  #not sure why this test is true should be false based on model user realtionship
+
+  # it "It is not valid when username is not unique" do
+  #
+  #   same_username = User.new(username: @user1.username)
+
+  #   binding.pry
+  #   expect(same_username).must_equal false
+  #
+  #
+  # end
+
+
+#not sure why this test is running, even though on website it does not allow it to be blank 
+  # it "It is not valid when the username is blank" do
+  #   blank_username = User.new()
+  #   binding.pry
+  #   expect(blank_username).must_equal false
+  # end
+
+
+
+
+
+
 
 
 
