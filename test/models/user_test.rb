1dd715a38301c89bde20d1ddb9d591f732a963f0
Carly Reiter <yepodacarly@gmail.com>
2018-10-14 20:12:14 -0700

    user model tests - iniital complete.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,46 @@
 require "test_helper"
+require "pry"
 
 describe User do
-  let(:user) { User.new }
+  describe 'validations' do
+    before do
+      @user = User.new(
+        name: 'Charles',
+      )
+    end
 
-  it "must be valid" do
-    value(user).must_be :valid?
+    it 'is valid when name is present' do
+      is_valid = @user.valid?
+      expect(is_valid).must_equal true
+
+    end
+
+    it 'is invalid without a name' do
+      @user.name = nil
+
+      is_valid = @user.valid?
+
+      expect(is_valid).must_equal false
+      expect(@user.errors.messages ).must_include :name
+    end
+
+
+    it 'is invalid with a non-unique name' do
+      @user.name = User.first.name
+
+      is_valid = @user.valid?
+
+      expect( is_valid ).must_equal false
+      expect( @user.errors.messages ).must_include :name
+    end
+
   end
+
+  describe "relations" do
+    it "has a vote" do
+      user = users(:jim)
+      user.votes.must_include votes(:one)
+    end
+  end
+
 end
