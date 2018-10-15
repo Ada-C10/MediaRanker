b8dc9bb7b7ffea2fbfd3de639e00d0766c8cbfa0
Chantelle Belic <chantelle.belic@gmail.com>
2018-10-14 18:56:38 -0700

    fixed redirect link by getting rid of status: 400 in redirect_back in votes contoller

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,17 +4,17 @@ class VotesController < ApplicationController
 
     user = User.find_by(id: session[:user_id])
     work = Work.find_by(id: params[:work_id])
-    
+
     @vote = Vote.new(user: user, work: work)
     if @vote.save
       flash[:success] = "Successfully upvoted!"
       redirect_back fallback_location: root_path
     elsif !user
       flash[:failure] = "A problem occurred: You must log in to do that."
-      redirect_back fallback_location: root_path, status: 400
+      redirect_back fallback_location: root_path
     else
       flash[:failure] = "A problem occurred: Could not upvote."
-      redirect_back fallback_location: root_path, status: 400
+      redirect_back fallback_location: root_path
     end
   end
 end
