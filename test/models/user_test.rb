f3a3ca43c581d45f0b3cce28d14d59d21b79cd78
Maryam Shitu <maryamshitu@gmail.com>
2018-10-15 00:35:50 -0700

    username validation test passing

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,4 +6,14 @@ describe User do
   it "must be valid" do
     value(user).must_be :valid?
   end
+
+  describe "Validations" do
+    it "must have a username" do
+      expect(user).must_respond_to :username
+
+      new_user = User.new(username: nil)
+      assert_not new_user.valid?
+    end
+
+  end
 end
