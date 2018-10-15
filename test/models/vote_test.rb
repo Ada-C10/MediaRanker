eb30d8d3ff391dad4727232a6746bb74a98f0d8c
Katricia Smith <smithkatricia@gmail.com>
2018-10-15 04:59:34 -0700

    Add vote model validation tests

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,9 +1,47 @@
 require "test_helper"
 
 describe Vote do
-  let(:vote) { Vote.new }
+  describe 'validations' do
 
-  it "must be valid" do
-    value(vote).must_be :valid?
+    it "is valid with all fields present" do
+      vote = votes(:one)
+      result = vote.valid?
+
+      expect(result).must_equal true
+    end
+
+    it "is invalid if user_id is nil" do
+      vote = votes(:two)
+      vote.user_id = nil
+
+      result = vote.valid?
+
+      expect(result).must_equal false
+      expect(vote.errors.messages).must_include :user
+    end
+
+    it "is invalid if work_id is nil" do
+      vote = votes(:two)
+      vote.work_id = nil
+
+      result = vote.valid?
+
+      expect(result).must_equal false
+      expect(vote.errors.messages).must_include :work
+    end
+
+    it "is invalid if both work_id and user_id are not present" do
+    vote = votes(:two)
+
+    vote.work_id = nil
+    vote.user_id = nil
+
+    vote.valid?.must_equal false
+    vote.errors.messages.must_include :work
+    vote.errors.messages.must_include :user
+    end
+
+    # uniqueness
+
   end
 end
