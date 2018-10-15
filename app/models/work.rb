ff325a57e04ecd7aa31c854f1bba4a0fe789ecd8
Katricia Smith <smithkatricia@gmail.com>
2018-10-15 02:06:33 -0700

    Add votes_count method to work model as helper method for vote sorting

--- app/models/work.rb
+++ app/models/work.rb
@@ -6,32 +6,25 @@ class Work < ApplicationRecord
   validates :title, presence: true, uniqueness: true
   validates :category, presence: true, inclusion: { in: WORK_CATEGORIES}
 
+
+  def votes_count
+    return self.votes.count
+  end
+
   def self.get_category_media(category)
     works = Work.where(category: category).to_a
     return works
   end
 
   def self.get_top_list(category)
-    # works.each do |work|
-      # works.sort_by {|item| item.votes}
-      # index = 0
-      # top_works = []
-      #
-      # while index < num
-      #   top_works < list[index]
-      #   index += 1
-      # end
-      #
-      # return top_works
-
-      category_works = Work.get_category_media(category).to_a
-      sorted = category_works.sort_by{|item| item[:votes]}
-      return sorted[0..9]
+    category_works = Work.get_category_media(category).to_a
+    sorted = category_works.sort_by{|item| item.votes_count}.reverse
+    return sorted[0..9]
     end
 
   def self.get_top_work
     works = Work.all.to_a
-    highest= works.sort_by{|item| item[:votes]}.reverse
+    highest= works.sort_by{|item| item.votes_count}.reverse
     return highest.first
   end
 
