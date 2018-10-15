c0867352666350439ae6fa1e0d9ed8e487da5277
Sigrid Benezra <sdbenezra@gmail.com>
2018-10-14 19:48:55 -0700

    Fix voting functionality to allow voting without voting twice.

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,26 +4,34 @@ class VotesController < ApplicationController
   end
 
   def new
-    user = User.find_by(id: session[:user_id])
+    user = @logged_in_user
     work = Work.find_by(id: params[:work_id])
     if user
-      @vote = @work.votes.new(user_id: user.id, work_id: work_id)
+      vote = work.votes.new(user_id: user.id, work_id: work.id)
     else
       flash[:error] = "Must be logged in to vote."
     end
   end
 
   def create
-    work = Work.find_by(id: params[:work_id])
-    user = User.find_by(id: session[:user_id])
-    @vote = Vote.new
-    # raise
-    if @vote.save
-      flash[:success] = "Successfully upvoted!"
-      redirect_to work_path(params[:work_id])
-    elsif
-      user == nil
+    user = @logged_in_user
+    if user == nil
       flash[:error] = "Must be logged in to vote."
+      redirect_to work_path(params[:work_id])
+    end
+
+    work = Work.find_by(id: params[:work_id])
+
+    work.votes.each do |vote|
+      if vote.user_id == :user_id
+        flash[:error] = "You can only vote once for a work."
+        redirect_back(fallback_location: work_path(params[:work_id]))
+      end
+    end
+
+    vote = work.votes.new(user_id: user.id, work_id: work.id)
+    if vote.save
+      flash[:success] = "Successfully upvoted!"
       redirect_to work_path(params[:work_id])
     else
       flash[:error] = "You can only vote once for a work."
