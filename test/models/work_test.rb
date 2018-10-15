0fd484ca9ca3021276dab2675958d8538142f33c
Danielle Metzner <danimetzner@gmail.com>
2018-10-15 07:55:23 -0700

    Added more tests to work model

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -15,18 +15,18 @@ describe Work do
     end
   end
 
-  # describe 'Relationships' do
-  #   it 'can have many votes' do
-  #     # Arrange (done with let)
-  #     work = works(:harrypotter)
-  #     new_vote = votes(:voteone)
-  #     # Act
-  #     vote = work.votes
-  #
-  #     # Assert
-  #     expect(vote).must_be_instance_of Vote
-  #     expect(vote.id).must_equal work.votes_id
-  #   end
+  describe 'Relationships' do
+    it 'can have many votes' do
+      # Arrange (done with let)
+      work = works(:harrypotter)
+      vote1 = votes(:voteone)
+      # Act
+      vote = work.votes
+
+      # Assert
+      expect(vote).must_be_instance_of Vote
+      expect(vote.id).must_equal work.votes_id
+    end
   #
   #   it 'can have many users' do
   #     # Arrange, did with let
@@ -42,7 +42,7 @@ describe Work do
   #       expect(genre).must_be_instance_of Genre
   #     end
   #   end
-  #end
+  end
 
   describe 'validations' do
     it 'must have a title' do
@@ -60,20 +60,29 @@ describe Work do
       expect(work.errors.messages[:title]).must_equal ["can't be blank"]
     end
 
+    it 'cannot have category other than movie, album or book' do
+      # Arrange
+      work = works(:harrypotter)
+
+      [nil, "art", 123, "indie"].each do |cat|
+        work.category = cat
+        valid = work.save
+        expect(valid).must_equal false
+        expect(work.errors.messages).must_include :category
+      end
+    end
+
     it 'must have category movie, album or book' do
       # Arrange
       work = works(:harrypotter)
-      work.category = 'art'
 
-      # Act
-      #valid = book.valid?
-      valid = work.save
+      ["movie", "album", "book"].each do |cat|
+        work.category = cat
+        valid = work.save
+        expect(valid).must_equal true
+      end
 
-      # Assert
-      expect(valid).must_equal false
-      expect(work.errors.messages).must_include :category
     end
-
 
     it 'requires a unique title in category' do
       #other_book = book.clone
@@ -86,7 +95,8 @@ describe Work do
       expect(valid).must_equal false
       expect(other_work.errors.messages).must_include :title
     end
-    it 'will allow a title that already exits if its in a different category' do
+
+    it 'will allow a title that already exists if its in a different category' do
       #other_book = book.clone
       other_work = works(:pride)
       other_work.title = work.title
@@ -94,6 +104,34 @@ describe Work do
       valid = other_work.valid?
 
       expect(valid).must_equal true
+    end
+  end
+
+  describe 'Media Page - Methods' do
+    it 'return top 10 of a given category' do
+      ten_movies = Work.top_movies
+      expect(ten_movies.length).must_equal 10
+      ten_movies.each do |movie|
+        expect(movie.category).must_equal "movie"
+      end
+    end
+
+    it 'will only return 10 if there are more than 10 in a category' do
+      new_movie = Work.new
+      new_movie.title = "some movie"
+      new_movie.category = "movie"
+      new_movie.save
+
+      ten_movies = Work.top_movies
+
+      expect(ten_movies.length).must_equal 10
+    end
+
+    it 'will only return less than 10 works if there are less than 10 in the category' do
+
+      ten_books = Work.top_books
+
+      expect(ten_books.length).must_equal 1
     end
   end
 end
