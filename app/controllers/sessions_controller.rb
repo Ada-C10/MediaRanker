a0404dfffc7fc7ca1db8cf53458f61d90b827ca8
Katrina Aganon <katrina.aganon@gmail.com>
2018-10-14 19:30:32 -0700

    updated forms with partial views

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,27 +1,27 @@
 class SessionsController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def login
     name = params[:username]
-    user = User.find_by(username: name)
+    @user = User.find_by(username: name)
 
-    if user
+    if @user
       flash[:success] = "Successfully logged in as #{name}"
-      session[:user_id] = user.id
+      session[:user_id] = @user.id
       redirect_to root_path
     else
-      user = User.new(username: name)
-      user.save
+      @user = User.new(username: name)
 
-      if user.save
+      if @user.save
         flash[:success] = "Successfully created new user #{name}"
-        session[:user_id] = user.id
+        session[:user_id] = @user.id
         redirect_to root_path
       else
-        flash.now[:error] = "Something went wrong, could not create user  #{name}"
-        render :new
+        flash.now[:error] = "Something went wrong, could not create user #{name}"
+        render :new, status: :bad_request
       end
 
     end
