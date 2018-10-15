7bb85e7e2693568182287ae4455e71a5a6bcf4a4
Chantelle Belic <chantelle.belic@gmail.com>
2018-10-14 17:51:17 -0700

    fixed some styling, applicationcontroller filter name from find_user to find_current_user and somehow that fixed the issue of login/logout not working on user#index and #show

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
 
-  before_action :find_user, :find_votes
+  before_action :find_current_user, :find_votes
 
   private
 
-  def find_user
+  def find_current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
 
