97ef4a70cd7839c277f815b8007f61c2efadf4b6
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-10-15 16:23:15 -0700

    Refactored vote_test with fixtures

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,70 +1,59 @@
 require "test_helper"
 
 describe Vote do
-  describe 'Vote validations' do
-    before do
-      @user = User.create!(name: 'test_name')
-      @work = Work.create!(title: 'test book', category: :album,
-        creator: 'test creator', publication: 2018,
-        description: 'test description')
-      @vote = Vote.new(user_id: @user.id, work_id: @work.id)
-    end
+  let (:vote) do
+    Vote.create!(user_id: users(:stevonnie).id, work_id: works(:parable).id)
+  end
 
+  describe 'Vote validations' do
     it 'is valid when all fields are present' do
-      expect(@vote.valid?).must_equal true
+      expect(vote.valid?).must_equal true
     end
 
     it 'has unique user_id within scope work' do
-      @vote.save
-      expect(@vote.valid?).must_equal true
-      another_vote = Vote.new(user_id: @user.id, work_id: @work.id)
-      expect(another_vote.valid?).must_equal false
-      expect(@vote.user_id).must_equal another_vote.user_id
-      expect(@vote.work_id).must_equal another_vote.work_id
+      # Expect valid when the user is a duplicate that's NOT within the scope work
+      valid_vote = Vote.new(user_id: users(:stevonnie).id, work_id: works(:valid_nil).id)
+      expect(valid_vote.valid?).must_equal true
+
+      # Expect invalid if user is a duplicate within the scope work
+      vote.save!
+      non_unique_vote = Vote.new(user_id: users(:stevonnie).id, work_id: works(:parable).id)
+      expect(non_unique_vote.valid?).must_equal false
     end
   end
 
   describe 'Vote relations' do
-    before do
-      @user = User.create!(name: 'test_name')
-      @work = Work.create!(title: 'test book', category: :album,
-        creator: 'test creator', publication: 2018,
-        description: 'test description')
-      @vote = Vote.new(user_id: @user.id, work_id: @work.id)
-    end
-
     # belongs_to triggers validation error by default
     it 'is invalid when user_id or work_id are not present' do
-      @vote.user_id = nil
-      expect(@vote.valid?).must_equal false
-      expect(@vote.errors.messages).must_include :user
+      vote.user_id = nil
+      expect(vote.valid?).must_equal false
+      expect(vote.errors.messages).must_include :user
 
-      @vote.work_id = nil
-      expect(@vote.valid?).must_equal false
-      expect(@vote.errors.messages).must_include :work
+      vote.work_id = nil
+      expect(vote.valid?).must_equal false
+      expect(vote.errors.messages).must_include :work
     end
 
     it 'can get work with \'work\'' do
-      expect(@vote.work).must_equal Work.find(@work.id)
+      expect(vote.work).must_equal Work.find(works(:parable).id)
     end
 
     it 'can get user with \'user\'' do
-      expect(@vote.user).must_equal User.find(@user.id)
+      expect(vote.user).must_equal User.find(users(:stevonnie).id)
     end
 
-# TODO: prune this down
     it 'deleting a work deletes the associated vote(s)' do
-      @vote.save
-      @second_user = User.create!(name: 'user_two')
-      @second_vote = Vote.create(user_id: @second_user.id, work_id: @work.id)
-      expect(@work.votes[1].user_id).must_equal @second_user.id
-      expect(@work.votes.length).must_equal 2
-      expect(@work.votes.find_by(user_id: @user.id)).must_equal @vote
-      expect(@work.votes.find_by(user_id: @second_user.id)).must_equal @second_vote
-      @work.destroy!
-      expect(@work.votes.length).must_equal 0
-      expect(@work.votes.find_by(user_id: @user.id)).must_equal nil
-      expect(@work.votes.find_by(user_id: @second_user.id)).must_equal nil
+      vote.save!
+      parable = works(:parable)
+      second_vote = Vote.create!(user_id: users(:pink).id, work_id: parable.id)
+      expect(parable.votes).must_equal [vote, second_vote]
+      expect(parable.votes.find_by(user_id: users(:stevonnie).id)).must_equal vote
+      expect(parable.votes.find_by(user_id: users(:pink).id)).must_equal second_vote
+
+      parable.destroy!
+
+      expect(users(:pink).votes).must_equal []
+      expect(users(:stevonnie).votes).must_equal []
     end
 
   end
