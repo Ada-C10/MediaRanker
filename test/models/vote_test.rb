d5c555eb8ea59082b153afae8bcee55fee011c8c
Layla <layla.bristol@gmail.com>
2018-10-14 17:40:00 -0700

    added test to check if a user already voted on a work

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -14,5 +14,20 @@ describe Vote do
       user = v.user
       expect(user).must_be_instance_of User
     end
+
+    it "a user can only vote on the same work once" do
+
+      v = Vote.first
+      work = v.work
+      user = v.user
+
+      has_voted = Vote.already_voted?(work.id, user.id)
+
+      expect(has_voted).must_equal true
+
+
+    end
+
+
   end
 end
