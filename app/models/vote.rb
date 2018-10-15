9c93c336282830c3cf5a5292e7d30a5e90854f4b
Katricia Smith <smithkatricia@gmail.com>
2018-10-15 05:20:47 -0700

    Edit vote uniqueness validation

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,8 +2,8 @@ class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :work
 
-  validates :work_id, presence: true
-  validates :work_id, uniqueness: true
+  validates :user_id, presence: true
+  validates :work_id, presence: true, uniqueness: { scope: :user_id }
   #
   # def upvote
   #   vote = Vote.new(work_id: @work.id, user_id: @logged_in_user.id)
--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -37,11 +37,35 @@ describe Vote do
     vote.user_id = nil
 
     vote.valid?.must_equal false
-    vote.errors.messages.must_include :work
+    vote.errors.messages.must_include :work_id
     vote.errors.messages.must_include :user
     end
 
-    # uniqueness
+    it "is invalid if both work_id and user_id are present in other Vote instance" do
 
+      vote = votes(:four)
+      vote2 = votes(:five)
+
+      vote2.work = vote.work
+
+      vote2.valid?.must_equal false
+      vote2.errors.messages.must_include :work_id
+    end
+
+    it "is valid if user is not unique" do
+      vote4 = votes(:four)
+      vote5 = votes(:five)
+
+    vote4.valid?.must_equal true
+    vote5.valid?.must_equal true
+    end
+
+    it "is valid if work is not unique" do
+      vote2 = votes(:two)
+      vote4 = votes(:four)
+
+    vote2.valid?.must_equal true
+    vote4.valid?.must_equal true
+    end
   end
 end
