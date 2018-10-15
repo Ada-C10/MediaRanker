f8edf5807451d8845d8babd90f0c4bb421c306b9
Katrina Aganon <katrina.aganon@gmail.com>
2018-10-14 20:07:01 -0700

    update rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,21 +3,17 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'works#welcome'
 
-  resources :works
-
   resources :works do
-    resources :votes
+    resources :votes, except:[:edit, :destroy]
   end
-  get '/works/welcome', to: 'works#welcome', as: 'welcome'
   post '/works/:id/upvote', to: 'works#upvote', as: 'upvote'
 
-  resources :users, except:[:create, :edit, :update] do
-    resources :votes
+  resources :users, only:[:index, :new, :show] do
+    resources :votes, except:[:edit, :destroy]
   end
 
   resources :sessions, only:[:new]
   post 'sessions/login', to: 'sessions#login', as: 'login'
   post 'sessions/logout', to: 'sessions#logout', as: 'logout'
-
 
 end
