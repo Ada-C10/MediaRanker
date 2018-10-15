4c768913dc1fdcbe73b4d7b23351964d1476ae10
Amber Lynn <griffiam@gmail.com>
2018-10-15 11:55:02 -0700

    fixed links for show and detail pages

--- test/controllers/works_controller_test.rb
+++ test/controllers/works_controller_test.rb
@@ -1,5 +1,20 @@
 require "test_helper"
 
 describe WorksController do
+  it "should respond with not found for showing a non-existant book" do
+    #ARRANGE
+    work = works(:work6)
+    id = work.id
 
+    get work_path(id)
+    must_respond_with :success
+
+    work.destroy
+
+    #ACT
+    get work_path(id)
+
+    #ASSERT
+    must_respond_with :missing
+  end
 end
