0be98bb374e80f0099658056b5616042ef8908e1
Jazz <jazziesf21@yahoo.com>
2018-10-14 20:29:50 -0700

    fixed formatting

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,7 +2,7 @@ class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :work
 
-  validates :work_id, uniqueness: { scope: :user_id, message: "You can only vote once for this work" }
+  # validates :work_id, uniqueness: { scope: :user_id, message: "You can only vote once for this work" }
 
 
 end
