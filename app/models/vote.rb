0dfd83037ce74daf49c13b1eb8032edacb10c582
Katricia Smith <smithkatricia@gmail.com>
2018-10-15 03:23:14 -0700

    Add validation for work_id uniqueness to vote model

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,6 +3,7 @@ class Vote < ApplicationRecord
   belongs_to :work
 
   validates :work_id, presence: true
+  validates :work_id, uniqueness: true
   #
   # def upvote
   #   vote = Vote.new(work_id: @work.id, user_id: @logged_in_user.id)
