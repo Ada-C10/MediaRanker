bf354dc3a820bca4880717fc76b2abb501912889
Laura <larmartinez2@gmail.com>
2018-10-15 14:47:53 -0700

    show page for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,5 +10,11 @@ class UsersController < ApplicationController
   # end
 
   def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  private
+  def user_params
+    return params.require(:user).permit(:username, :id)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   # has_many :works, through: :votes
   validates :username, presence: true, uniqueness: true
   has_many :votes
+
+  def joining_date
+    return self.created_at
+  end
 end
