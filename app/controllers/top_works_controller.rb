a4b1356389435e465c6bcd30315fdc2a6622999c
Katricia Smith <smithkatricia@gmail.com>
2018-10-15 02:19:08 -0700

    Delete old function call commented out

--- app/controllers/top_works_controller.rb
+++ app/controllers/top_works_controller.rb
@@ -3,7 +3,6 @@ class TopWorksController < ApplicationController
   before_action :find_logged_in_user
 
   def index
-    # @top = Work.get_top_work(Work.all)
     @top = Work.get_top_work
     #
     # @top_ten = Work.get_top_list
