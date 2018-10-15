16a4fee2a7158c6063a4f3b5ff631d2a861c7de6
Katricia Smith <smithkatricia@gmail.com>
2018-10-15 01:48:24 -0700

    Add vote handling methods and html

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,9 +2,10 @@ class Vote < ApplicationRecord
   belongs_to :user
   belongs_to :work
 
-  def upvote
-    vote = Vote.new(work_id: @work.id, user_id: @logged_in_user.id)
-    # .votes has to be called on an instance, not on the class
-  end
+  validates :work_id, presence: true
+  #
+  # def upvote
+  #   vote = Vote.new(work_id: @work.id, user_id: @logged_in_user.id)
+  # end
 
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -5,4 +5,34 @@ class Work < ApplicationRecord
   has_many :votes
   validates :title, presence: true, uniqueness: true
   validates :category, presence: true, inclusion: { in: WORK_CATEGORIES}
+
+  def self.get_category_media(category)
+    works = Work.where(category: category).to_a
+    return works
+  end
+
+  def self.get_top_list(category)
+    # works.each do |work|
+      # works.sort_by {|item| item.votes}
+      # index = 0
+      # top_works = []
+      #
+      # while index < num
+      #   top_works < list[index]
+      #   index += 1
+      # end
+      #
+      # return top_works
+
+      category_works = Work.get_category_media(category).to_a
+      sorted = category_works.sort_by{|item| item[:votes]}
+      return sorted[0..9]
+    end
+
+  def self.get_top_work
+    works = Work.all.to_a
+    highest= works.sort_by{|item| item[:votes]}.reverse
+    return highest.first
+  end
+
 end
