e231a22853c7f6ca3bd93d8d9e1a5090c10c3fef
Naheed Arang <arang.na@gmail.com>
2018-10-15 19:02:44 -0700

    add home controller, add some styling

--- app/models/work.rb
+++ app/models/work.rb
@@ -2,18 +2,18 @@ class Work < ApplicationRecord
   has_many :votes, dependent: :destroy
   validates :title, presence: true
 
-  def sort_works(category)
+  def self.sort_works(category)
     works = Work.where(category: category)
     sorted_works = works.sort_by {|work| work.votes.count}
-    return sorted.works.reverse
+    return sorted_works.reverse
   end
 
-  def list_top_ten(category)
+  def self.list_top_ten(category)
     works = Work.sort_works(category)
     return works[0..9]
   end
 
-  def find_top_work
+  def self.find_top_work
     works = Work.all
     work = works.sort_by {|work| work.votes.count}
     return work[-1]
