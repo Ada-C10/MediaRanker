c97fb4dce7ddd64dcce21363e0e0f51d4cc6f599
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-10-16 11:24:39 -0700

    Seed data test is not alphabatizing correctly

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -103,8 +103,39 @@ describe Work do
       end
 
       it "sorts test(seed) data correctly" do
+
+        require 'csv'
+
+        WORK_FILE = Rails.root.join('db', 'media_seeds.csv')
+        puts "Loading raw media (work) data from #{WORK_FILE}"
+
+        work_failures = []
+        CSV.foreach(WORK_FILE, :headers => true) do |row|
+          work = Work.new
+          work.title = row['title']
+          work.category = row['category']
+          work.creator = row['creator']
+          work.publication = row['publication_year'].to_i
+          work.description = row['description']
+          successful = work.save
+          if !successful
+            work_failures << work
+            puts "Failed to save work: #{work.inspect}"
+          else
+            puts "Created work: #{work.inspect}"
+          end
+        end
+
+        puts "Added #{Work.count} work records"
+        puts "#{work_failures.length} works failed to save"
+
         hash_keys = ["album", "book", "movie"]
         expect(works_hash.keys).must_equal hash_keys
+
+        works_hash["book"].each do |book|
+          p book.votes.count
+          p book.title
+        end
 
         hash_keys.each do |category|
           last_index = works_hash[category].length-1
