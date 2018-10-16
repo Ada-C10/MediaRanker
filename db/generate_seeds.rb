85c6a29b782c1c5db02f82948d9aedbcc26d544f
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-10-16 11:24:39 -0700

    New test for work model passes

--- db/generate_seeds.rb
+++ db/generate_seeds.rb
@@ -13,7 +13,7 @@ require 'csv'
 CSV.open('db/media_seeds.csv', "w", :write_headers=> true,
   :headers => ["category", "title", "creator", "publication_year", "description"]) do |csv|
 
-  5.times do
+  25.times do
     category = %w(album book).sample
     title = Faker::Coffee.blend_name
     creator = Faker::Name.name
--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -5,6 +5,7 @@ describe Work do
   let (:parable) { works(:parable) }
   let (:deluxe) { works(:deluxe) }
   let (:parable_album) { works(:parable_album) }
+  let (:circa) { works(:circa) }
   let (:onion) { users(:onion) }
 
   describe 'Work validations' do
@@ -51,41 +52,89 @@ describe Work do
     end
 
     it 'can get users with \"work.users\" (through votes)' do
-      expect(work.users).must_equal User.where.not(name: "onion")
+      expect(work.users).must_equal User.where.not(name: "onion").where.not(name: "lars-of-the-stars")
     end
   end
 
   describe 'Work model methods: listing and sorting' do
-    it "most_recent_vote_date returns the youngest vote" do
-      # Create a new vote -> expect most recent vote to be today's
-      Vote.create!(user: onion, work: parable)
-      expect(Work.find_by(title: "Parable of the Sower").most_recent_vote_date.to_date).must_equal Date.today
-    end
+    describe 'Helper methods' do
+      it "most_recent_vote_date returns the youngest vote" do
+        # Create a new vote -> expect most recent vote to be today's
+        Vote.create!(user: onion, work: parable)
+        expect(Work.find_by(title: "Parable of the Sower").most_recent_vote_date.to_date).must_equal Date.today
+      end
 
-    it "most_recent_vote_date returns a specific, ancient date for a work with no votes" do
-      expect(parable_album.most_recent_vote_date).must_equal Date.jd(0)
-    end
+      it "most_recent_vote_date returns a specific, ancient date for a work with no votes" do
+        expect(parable_album.most_recent_vote_date).must_equal Date.jd(0)
+      end
 
-    it "#self.sort_by_most_recent_vote correctly sorts in ascending order" do
-      # Create a new vote today:
-      Vote.create!(user: onion, work: deluxe)
-      sorted_works = Work.sort_by_most_recent_vote(Work.all.to_a)
+      it "#self.sort_by_most_recent_vote correctly sorts in ascending order" do
+        # Create a new vote today:
+        Vote.create!(user: onion, work: deluxe)
+        sorted_works = Work.sort_by_most_recent_vote(Work.all.to_a)
 
-      sorted_works.each_with_index do |work, index|
-        if index < sorted_works.length - 1
-          expect(work.most_recent_vote_date).must_be :<=, sorted_works[index+1].most_recent_vote_date
-        else
-          expect(work.most_recent_vote_date.to_date).must_equal Date.today
+        sorted_works.each_with_index do |work, index|
+          if index < sorted_works.length - 1
+            expect(work.most_recent_vote_date).must_be :<=, sorted_works[index+1].most_recent_vote_date
+          else
+            expect(work.most_recent_vote_date.to_date).must_equal Date.today
+          end
         end
       end
     end
 
-    it "self.list_all_works" do
-      
+    describe 'self.list_all_works' do
+      let(:works_hash){Work.list_all_works}
+      # after .reverse!, the final sort order is:
+      # 1. most to fewest votes,
+      # 2. newest to oldest vote,
+      # 3. A to Z (by title)
+      it "orders correctly" do
+        # make all of the works a book except parable_album
+        [deluxe, circa].each do |work|
+          work.category = "book"
+          work.save!
+        end
+
+        expected_order = [deluxe, parable, circa]
+        expected_order.each_with_index do |title, index|
+          expect(works_hash["book"][index]).must_equal title
+        end
+      end
+
+      it "sorts test(seed) data correctly" do
+        hash_keys = ["album", "book", "movie"]
+        expect(works_hash.keys).must_equal hash_keys
+
+        hash_keys.each do |category|
+          last_index = works_hash[category].length-1
+
+          works_hash[category].each_with_index do |work, index|
+            if index < last_index
+              next_work = works_hash[category][index+1]
+              expect(work.votes.size).must_be :>=, next_work.votes.size
+
+              if work.votes.count == next_work.votes.count
+                expect(work.most_recent_vote_date).must_be :>=, next_work.most_recent_vote_date
+
+                if work.most_recent_vote_date == next_work.most_recent_vote_date
+                  expect(work.title).must_be :<=, next_work.title
+                end
+
+              end
+            end
+
+          end
+        end
+
+      end
     end
 
-    it "self.list_top_works" do
 
+    describe 'self.list_top_works' do
+      it "does what i want" do
+
+      end
     end
   end
 end
