01c33274266180572f914b024038e6f34fe4119b
Shelan <shelandebesai@gmail.com>
2018-10-18 06:52:07 -0700

    updated work's model, homepage and layout

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,42 +1,57 @@
 class WorksController < ApplicationController
+
+  before_action :category_from_work, except: [:root, :index, :new, :create]
+  #lets you know the category of work before running controller action
+  def root
+    @albums = Work.highest_albums #highest votes for album
+    @books = Work.highest_books #highest votes for book
+    @movies = Work.highest_movies #highest votes for movie
+    @best_work = Work.order(vote_count: :desc).first #highest votes overall
+  end
+
   def index
-    @works = Work.all
+    @works_by_category = Work.to_category_hash #Work's category
+    # @works = Work.all
   end
 
   def show
     id = params[:id].to_i
-    @work = Work.find(params[:id].to_i)
+    @work = Work.find(params[:id].to_i) #if work id is found, assign to @work
+    @votes = @work.votes.order(created_at: :desc) #@votes = work's votes in descending order
 
-    if id.nil?
-      render :notfound
-    end
+    # if id.nil?
+    #   render :notfound
+    # end
   end
 
   def new
-    @work = Work.new
+    @work = Work.new #new work
   end
 
   def edit
-    id = params[:id].to_i
-    @work = Work.find(params[:id].to_i)
+    id = params[:id].to_i #work's id
+    @work = Work.find(params[:id].to_i) #if found, assign it to @work
   end
 
   def update
-    id = params[:id].to_i
-    @work = Work.find_by(id: id)
-    @work.update(work_params)
+    id = params[:id].to_i #work's id
+    @work = Work.find_by(id: id) #if found, assign it to @work
+    @work.update(work_params) #update work
 
     if @work.save
+      flash[:success] = "#{@work.category} is updated!" #success msg displayed of saved
       redirect_to works_path
     else
+      flash.now[:error] = "Not updated!" #error msg displayed if work is not saved due to validations
       render :edit
     end
   end
 
   def create
     @work = Work.new(work_params)
+    @work_category = @work.category
     if @work.save # save returns true if the database insert succeeds
-      flash[:success] = "#{@work.category} is created!"
+      flash[:success] = "#{@work_category} is created!"
 
       redirect_to root_path # for now
     else # save failed :(
@@ -54,7 +69,7 @@ class WorksController < ApplicationController
     id = params[:id].to_i
     @work = Work.find_by(id: id)
     if @work.destroy
-      flash[:success] = "#{@work.title} is deleted"
+      flash[:success] = "#{@work_category} is deleted"
       redirect_to root_path
     end
   end
@@ -76,9 +91,20 @@ class WorksController < ApplicationController
   # end
 
 
+
+
+
   private
 
   def work_params
     return params.require(:work).permit(:category, :title, :creator, :published_year, :description)
+  end
+
+  def category_from_work
+    if @work = Work.find_by(id: params[:id]) #if work id is found...
+      @work_category = @work.category.downcase.pluralize #work's category is downcased and pluralize and assigned to @work_category
+    else
+      flash.now[:error] = "Error!"
+    end
   end
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,30 +1,59 @@
 class Work < ApplicationRecord
+  CATEGORIES = %w(album book movie) #store categories in a constant
   has_many :votes
   has_many :users, through: :votes
 
+
   def upvote user
-    @vote = Vote.new
-    @vote.work_id = self.id
-    @vote.user_id = user.id
-    @vote.vote_time = Date.today
-    unless @vote.valid?
-      puts "*" * 80
-      puts "#{@vote.errors.messages}"
+    @vote = Vote.new #create a new vote
+    @vote.work_id = self.id #a work's id is assigned vote's foreign key, work_id
+    @vote.user_id = user.id #a user's id is assigned to vote's foreign key, user_id
+    @vote.vote_time = Date.today #vote's date
+    unless @vote.valid? #if vote is not valid...
+      puts "#{@vote.errors.messages}" #display error messages
     end
-    @vote.save
+    @vote.save #save vote so user cannot duplicate vote for a work
   end
 
-  def self.sorted(category)
-    works = Work.all.where(category: category).to_a
-    return works.sort_by { |work| work.total_votes }
+  def self.to_category_hash #work.to_category
+    data = {}
+    CATEGORIES.each do |cat| #iterates over each category
+      data[cat] = by_category(cat) #data[album, book, movie] = by_category(album, book, movie)
+    end
+    return data #return data = {album, book, movie}
   end
 
-  def self.top_10(category)
-    return Work.sorted_list(category)[0..9]
+  def self.by_category(category) #sorts works by category
+    category = category.singularize.downcase
+    self.where(category: category).order(vote_count: :desc) #where is work's category based on vote count (largest to smallest)?
   end
 
-  def self.top
-    works = Work.all.to_a
-    return works.max_by { |work| work.total_votes }
+  def self.top_ten(category) #calls 10 works based on chosen category
+    where(category: category).order(vote_count: :desc)[0..10] #descending order
   end
+
+
+  # def self.top
+  #   works = Work.all.to_a
+  #   return works.max_by { |work| work.total_votes }
+  # end
+
+  def self.highest_albums #call self.top_10 method for album category
+    top_ten("album")  #displays up to 10 albums
+  end
+
+  def self.highest_books #call self.top_10 method for book category
+    top_ten("book") #displays up to 10 books
+  end
+
+  def self.highest_movies #call self.top_10 method for movie category
+    top_ten("movie")  #displays up to 10 movies
+  end
+
+
+
+
+
+
+
 end
